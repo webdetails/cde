b6e08ab59cbe898602007524c028e5ae42c7236e
tgf <tiago.ferreira@webdetails.pt>
2011-01-17 15:20:59 +0000

    messages.properties -> /lang/messages.properties

--- server/plugin/src/pt/webdetails/cdf/dd/Messages.java
+++ server/plugin/src/pt/webdetails/cdf/dd/Messages.java
@@ -32,7 +32,8 @@ public class Messages {
     if (bundle == null) {
       IPentahoSession session = new StandaloneSession( "dashboards messages" ); //$NON-NLS-1$
         try {
-          InputStream in = PentahoSystem.get(ISolutionRepository.class, session).getResourceInputStream("system/"+DashboardDesignerContentGenerator.PLUGIN_NAME+"/messages.properties", true); //$NON-NLS-1$ //$NON-NLS-2$
+          String messagesPath = "system/"+DashboardDesignerContentGenerator.PLUGIN_NAME+"/lang/messages.properties";//$NON-NLS-1$ //$NON-NLS-2$
+          InputStream in = PentahoSystem.get(ISolutionRepository.class, session).getResourceInputStream(messagesPath, true, ISolutionRepository.ACTION_EXECUTE); 
           bundle = new PropertyResourceBundle( in );
           Messages.locales.put(locale, bundle);
         } catch (Exception e) {
