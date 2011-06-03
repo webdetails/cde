e17b1e5aea21ccde91e4f8644c60087fd44d5283
Pedro Alves <pmgalves@gmail.com>
2011-06-03 15:32:00 +0100

    - Added missing classes for the new render types

---
+++ server/plugin/src/pt/webdetails/cdf/dd/render/layout/ResourceCodeRender.java
@@ -0,0 +1,14 @@
+package pt.webdetails.cdf.dd.render.layout;
+
+import org.apache.commons.jxpath.JXPathContext;
+
+/**
+ *
+ * @author pedro
+ */
+public class ResourceCodeRender extends ResourceRender
+{
+  public ResourceCodeRender(JXPathContext context) {
+        super(context);
+    }
+}
