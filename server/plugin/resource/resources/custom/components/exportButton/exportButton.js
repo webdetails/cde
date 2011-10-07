04f4637e778474b9b2527f8e158e214fe9ece532
Pedro Pinheiro <pdpinheiro@gmail.com>
2011-10-07 20:42:56 +0100

    Added export component

---
+++ server/plugin/resource/resources/custom/components/exportButton/exportButton.js
@@ -0,0 +1,24 @@
+
+
+
+var ExportButtonComponent = BaseComponent.extend({
+
+  ph: undefined,
+  tc: undefined,
+  
+/* BUILD THE COMPONENT */
+
+  update: function(){
+    
+    var myself = this;
+    $.extend(this.options,this);
+
+    this.ph = $("#" + this.htmlObject);
+    this.ph.empty();
+    var bar = $('<span class="exportButton"></span>').appendTo(this.ph);
+    var component = Dashboards.getComponentByName( "render_" + this.componentName );
+    var overrideParameters = Dashboards.propertiesArrayToObject(myself.parameters);    
+    bar.text( this.label ).click( function(){component.query.export(myself.outputType, overrideParameters)} );
+  }
+
+});
