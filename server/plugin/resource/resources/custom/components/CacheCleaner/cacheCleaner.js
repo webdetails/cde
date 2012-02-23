b20db405d5f802b7fe79ba56ca0094be76a58a00
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-02-23 19:11:46 +0000

    Update cache cleaner component

--- server/plugin/resource/resources/custom/components/CacheCleaner/cacheCleaner.js
+++ server/plugin/resource/resources/custom/components/CacheCleaner/cacheCleaner.js
@@ -5,9 +5,10 @@ CacheCleanerComponent = BaseComponent.extend({
    * 2. Cube
    * 3. Dimension
    * 4. Hierarchy
-   * 5+ Member (at whatever depth limit is set)
+   * 5+ Member (might be quite deep)
    */
   maxDepth: 4,
+  updateDelay: 200,
   breadcrumb: [],
   catalogs: [],
   dimensions: [],
@@ -35,8 +36,7 @@ CacheCleanerComponent = BaseComponent.extend({
         member = this.breadcrumb.length >= 4 ?
             this.breadcrumb[this.breadcrumb.length - 1]:
             null;
-    /*
-     * Bigger breadcrumbs mean deeper searches. First level
+    /* Bigger breadcrumbs mean deeper searches. First level
      * is catalog, second is cube, third and beyond is a
      * dimension or a specific member thereof.
      */
@@ -59,13 +59,21 @@ CacheCleanerComponent = BaseComponent.extend({
   },
 
   handleResponse: function(resp) {
+    /* We expect certain properties to be available
+     * on the response object, depending on the level
+     * we're browsing at. From those properties, we can
+     * determine what level the response refers to, and
+     * we build a standardized (index, value, label)
+     * object for each item in the response, which will
+     * then be used to draw the list.
+     */
     var opts, next;
     if (resp.catalogs) {
       this.catalogs = resp.catalogs;
       opts = resp.catalogs.map(function(e,i){
         return {
           index: i,
-          value: e.schema,
+          value: e.name,
           label: e.name
         };
       });
@@ -96,13 +104,16 @@ CacheCleanerComponent = BaseComponent.extend({
         }
       });  
     }
+    /* Hoorah, we have the item list,
+     * and we can now display it.  
+     */
     this.displayList(opts);
   },
 
   displayList: function(items) {
     var myself = this,
         $ph = $("<div class='cacheCleanerComponent'>").appendTo($("#" + this.htmlObject).empty()),
-        $title = $("<div class='title WDdataCell'>Your Selection</div>"),
+        $title = $("<li class='cacheItem title WDdataCell'>Your Selection</li>"),
         $list = $("<ul class='cacheItemList'>").appendTo($ph),
         title,
         levelIndex = Math.min(this.levelLabels.length,this.breadcrumb.length),
@@ -112,15 +123,20 @@ CacheCleanerComponent = BaseComponent.extend({
     if (this.breadcrumb.length < this.maxDepth) {
       $list.append("<li class='cacheItem WDdataCell'>Choose a "+levelLabel+ "</li>");
     }
-    /* Now add the actual items for our current level*/
+    /* Now add the actual items for our current level */
     $.each(items, function(i,e){
       myself.drawActiveItem($list,e,i);
     });
 
-    /* Show a label for the selection, but only if there's something to show */
-    if (myself.breadcrumb.length) {
-      $title.appendTo($list);
+    /* We only show the label for the selection only if
+     * there's an actual selection, but we still need it
+     * to exist for the animations.
+     */
+    $title.appendTo($list);
+    if (!myself.breadcrumb.length) {
+      $title.css('visibility','hidden');
     }
+    this.title = $title;
     $("<div class='button'>Reset</div>").click(function(){
       myself.breadcrumb = [];
       myself.update();
@@ -130,6 +146,14 @@ CacheCleanerComponent = BaseComponent.extend({
     $.each(this.breadcrumb.slice().reverse(), function(i,e){
         myself.drawBreadCrumbItem($list,e,i);
     });
+
+    /* Show the contents */
+    setTimeout(function(){$ph.css('opacity','1')},1);
+
+    /* please, sir, can we have a reference
+     * to the list from outside this method?
+     */
+     this.list = $list;
   },
 
   drawBreadCrumbItem: function($ph,item,index) {
@@ -138,12 +162,11 @@ CacheCleanerComponent = BaseComponent.extend({
         $refine;
     
     $item.text(item.label);
-    $("<div>Refine</div>").addClass(index == 0 ?
-        'fauxButton':
-        'button').click(
+    $("<div>Unselect</div>").addClass('button').click(
       function(){
-        myself.breadcrumb = myself.breadcrumb.slice(0,myself.breadcrumb.length - index);
-        myself.update();
+        myself.breadcrumb = myself.breadcrumb.slice(0,myself.breadcrumb.length - index - 1);
+        $item.css('top',myself.title.position().top - $item.position().top);
+        setTimeout(function(){myself.update();},myself.updateDelay);
       }).appendTo($item);
     $item.append("<div class='fauxButton'>URL</div>");
     $item.append("<div class='fauxButton'>Clean</div>");
@@ -157,27 +180,105 @@ CacheCleanerComponent = BaseComponent.extend({
     
     $item.text(item.label);
 
-    $("<div class='button'>Refine</div>").click(function(){
-      if(myself.breadcrumb.length < myself.maxDepth - 1) {
-        myself.breadcrumb.push(item);
-        myself.update();
-      }
-    }).appendTo($item);
+    $refine = $("<div class='button'>Refine</div>");
+ 
+   /* We only want the refine button to be active if
+    * we can drill down any further than we have
+    */
+    if (this.breadcrumb.length < this.maxDepth - 1) {
+      $refine.addClass('button').click(function(){
+        if(myself.breadcrumb.length < myself.maxDepth - 1) {
+          myself.breadcrumb.push(item);
+          $item.css('top',myself.title.position().top - $item.position().top);
+          setTimeout(function(){myself.update();},myself.updateDelay);
+        }
+      });
+    } else {
+      $refine.addClass('fauxButton');
+    }
+    $refine.appendTo($item);
     $("<div class='button'>URL</div>").click(function(){
-      myself.getURL(item);
+      myself.displayURL(item);
     }).appendTo($item);
     $("<div class='button'>Clean</div>").click(function(){
-      myself.clearCache(item);
+      myself.clearCache(item,this);
     }).appendTo($item);
     $item.appendTo($ph);
   },
 
-  clearCache: function(item) {
-    Dashboards.log('Requested cache clearing for ' + item.label);
+  clearCache: function(item,target) {
+    var $target = $(target);
+    $.ajax({
+      type: 'GET',
+      url: this.getURL(item),
+      success: function(response){
+        var text = $target.text();
+        $target.text('Done').addClass('success');
+        setTimeout(function(){$target.text(text).removeClass('success')},1100);
+      },
+      error: function(response, xhr, err) {
+        application.popupEngine.getPopup('close').show({
+          header:'Something went wrong',
+          content:'There was an issue clearing the cache. Please refer to the' + 
+                ' server logs for more information. The server response was: ' + 
+                err
+        });
+      }
+    });
   },
+
+  displayURL: function(item) {
+    var myself = this,
+        url = window.location.host + this.getURL(item),
+        $title = $("<li class='cacheItem title WDdataCell'>Clear the cache with this URL:</li>"),
+        $url = $("<li class='cacheItem'><textarea rows='2' cols='100'>"+this.getURL(item)+"</textarea></li>"),
+        ta = $url.find('textarea').get(0);
+
+ 
+    $("<div class='button'>Dismiss</div>").click(function() {
+        $url.css('height','0');
+        $url.css('min-height','0');
+        $url.css('padding','0');
+        $title.css('height','0');
+        $title.css('min-height','0');
+        $title.css('padding','0');
+      setTimeout(function(){
+        $url.remove();
+        $title.remove();
+      },myself.updateDelay);
+    }).appendTo($title);
+
+    if(this.$url){
+      this.$url.remove()
+      this.$urltitle.remove()
+    }
+    this.$url = $url;
+    this.$urltitle = $title;
+    this.list.prepend($url);
+    this.list.prepend($title);
+    ta.focus();
+    ta.select();
+  },
+
   getURL: function(item) {
-    Dashboards.log('Requested cache clearing for ' + item.label);
+    var webservice = 'MondrianCacheCleanService',
+        methods = ["clearCatalog","clearCube","clearDimension","clearHierarchy"],
+        parameters = ['catalog','cube','dimension','hierarchy'],
+        p, param,
+        urlParams = {},
+        urlMethod;
+
+    for (p = 0; p < parameters.length; p++ ) {
+        if(this.breadcrumb.length <= p) break;
+        param = parameters[p];
+        urlParams[param] = this.breadcrumb[p].value;
+    }
+    urlParams[parameters[p]] = item.value;
+    urlMethod = methods[p];
+    urlMethod = '/pentaho/content/ws-run/' + webservice + '/' + urlMethod;
+    return window.location.protocol + "//" + window.location.host + urlMethod + "?" +  $.param(urlParams);
   },
+
   cubeListing: function(callback) {
     $.getJSON("OlapUtils", {
         operation:"GetOlapCubes"
