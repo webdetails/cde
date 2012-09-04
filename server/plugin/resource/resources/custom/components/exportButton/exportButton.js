4b217c80d34f3602dbad75b2523a53e790bfea11
tgf <tiago.n.ferreira@gmail.com>
2012-09-04 15:35:20 +0100

    export button: when pointing to a datatable, use its filter

--- server/plugin/resource/resources/custom/components/exportButton/exportButton.js
+++ server/plugin/resource/resources/custom/components/exportButton/exportButton.js
@@ -17,10 +17,38 @@ var ExportButtonComponent = BaseComponent.extend({
     this.ph.empty();
     var bar = $('<span class="exportButton"></span>').appendTo(this.ph);
     var component = Dashboards.getComponentByName( "render_" + this.componentName );
+    
     var overrideParameters = Dashboards.propertiesArrayToObject(myself.parameters);    
+
     bar.text( this.label ).click( function(){
-      component.query.exportData(myself.outputType, overrideParameters);
+      component.query.exportData(myself.outputType, overrideParameters, myself.getFilterSettings(component));
     } );
+  },
+  
+  //add filtering to export if it's a data table
+  getFilterSettings: function(component){
+    var extraSettings = {};
+	if(component.type == "Table"){
+	  //var dtOptions = component.getTableOptions(component.chartDefinition);
+	  //if(dtOptions.bFilter) {
+	  var dtOptions = component.ph.dataTableSettings[0];
+	  if(dtOptions.oFeatures.bFilter) {
+		var searchInput = component.ph.find('input');
+		if(searchInput) {
+		  extraSettings.dtFilter = searchInput.val();
+		  if(dtOptions.aoColumns){
+			var idxs = [];
+		    for(var i=0; i< dtOptions.aoColumns.length;i++){
+			  if(dtOptions.aoColumns[i].bSearchable){
+			    idxs.push(i);
+		      }
+			}
+			extraSettings.dtSearchableColumns = idxs.join(',');
+		  }
+		}
+	  }
+	}
+	return extraSettings;
   }
 
 });
