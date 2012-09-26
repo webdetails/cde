6c7eb6959bcef36666e16edc13411d73ee5467f4
Pedro Vale <pedro.vale@webdetails.pt>
2012-09-26 11:20:05 +0100

    MPL license on some more headers

--- server/plugin/src/pt/webdetails/cdf/dd/Dashboard.java
+++ server/plugin/src/pt/webdetails/cdf/dd/Dashboard.java
@@ -1,16 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 package pt.webdetails.cdf.dd;
 
 import java.util.Date;
 import org.pentaho.platform.api.engine.IParameterProvider;
 
-/**
- *
- * @author pdpi
- */
+
 public interface Dashboard
 {
 
