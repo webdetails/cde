945a5d8439525db0fddf498f5b8e35f905094bb2
tgf <tgf@segafredo.(none)>
2013-01-21 16:43:11 +0000

    Concatenate: fix issue when missing file caused total failure; close streams, log errors

--- server/plugin/src/pt/webdetails/cdf/dd/packager/Concatenate.java
+++ server/plugin/src/pt/webdetails/cdf/dd/packager/Concatenate.java
@@ -1,7 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package pt.webdetails.cdf.dd.packager;
 
 import java.io.BufferedReader;
@@ -12,9 +12,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Enumeration;
 import java.util.NoSuchElementException;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  *
@@ -22,6 +27,7 @@ import org.apache.commons.lang.StringUtils;
  */
 class Concatenate
 {
+  private static final Log logger = LogFactory.getLog(Concatenate.class);
 
   public static InputStream concat(File[] files)
   {
@@ -36,52 +42,64 @@ class Concatenate
     {
       return concat(files);
     }
-    try
-    {
+
       StringBuffer buffer = new StringBuffer();
       for (File file : files)
       {
-        StringBuffer tmp = new StringBuffer();
-        BufferedReader fr = new BufferedReader(new FileReader(file));
-        while (fr.ready())
-        {
-          tmp.append(fr.readLine());
+        //TODO: review this!
+        BufferedReader fr = null;
+        try {
+          StringBuffer tmp = new StringBuffer();
+          fr = new BufferedReader(new FileReader(file));
+          while (fr.ready())
+          {
+            tmp.append(fr.readLine());
+          }
+          rootpath = rootpath.replaceAll("\\\\", "/").replaceAll("/+", "/");
+          // Quick and dirty hack: if the path aims at the custom components, we point at getResource, else we point at the static resource folders
+  
+          String filePath = file.getPath().replaceAll("\\\\", "/"); // Fix windows slashes'
+          String fileLocation = "";
+          if (filePath.contains("resources/custom"))
+          {
+            fileLocation = filePath.replaceAll(file.getName(), "") // Remove this file's name
+                    .replaceAll(rootpath, "../");   //
+            //fileLocation = "";
+          }
+          else if (filePath.matches(".*pentaho-cdf-dd/css/.*/.*$"))
+          {
+            fileLocation = filePath.replaceAll(file.getName(), "") // Remove this file's name
+                    .replaceAll(rootpath, "../");
+          }
+          else if (filePath.matches(".*cde/components/.*/.*$"))
+          {
+            fileLocation = "../../res/" + filePath.substring(filePath.indexOf("cde/components/")).replaceAll(file.getName() + "$", "");
+          }
+          buffer.append(tmp.toString() //
+                  // We need to replace all the URL formats
+                  .replaceAll("(url\\(['\"]?)", "$1" + fileLocation.replaceAll("/+", "/"))); // Standard URLs
         }
-        rootpath = rootpath.replaceAll("\\\\", "/").replaceAll("/+", "/");
-        // Quick and dirty hack: if the path aims at the custom components, we point at getResource, else we point at the static resource folders
-
-        String filePath = file.getPath().replaceAll("\\\\", "/"); // Fix windows slashes'
-        String fileLocation = "";
-        if (filePath.contains("resources/custom"))
-        {
-          fileLocation = filePath.replaceAll(file.getName(), "") // Remove this file's name
-                  .replaceAll(rootpath, "../");   //
-          //fileLocation = "";
+        catch (FileNotFoundException e) {
+          logger.error("concat: File " + file.getAbsolutePath() + " doesn't exist! Skipping...");
         }
-        else if (filePath.matches(".*pentaho-cdf-dd/css/.*/.*$"))
-        {
-          fileLocation = filePath.replaceAll(file.getName(), "") // Remove this file's name
-                  .replaceAll(rootpath, "../");
+        catch (Exception e) {
+          logger.error("concat: Error while attempting to concatenate file "
+              + file.getAbsolutePath() + ". Trying to continue...", e);
         }
-        else if (filePath.matches(".*cde/components/.*/.*$"))
-        {
-          fileLocation = "../../res/" + filePath.substring(filePath.indexOf("cde/components/")).replaceAll(file.getName() + "$", "");
+        finally {
+          IOUtils.closeQuietly(fr);
         }
-        buffer.append(tmp.toString() //
-                // We need to replace all the URL formats
-                .replaceAll("(url\\(['\"]?)", "$1" + fileLocation.replaceAll("/+", "/"))); // Standard URLs
-        //.replaceAll("(progid:DXImageTransform.Microsoft.AlphaImageLoader\\(src=')", "$1" + fileLocation + "../")); // these are IE-Only
-
 
       }
-      return new ByteArrayInputStream(buffer.toString().getBytes("UTF8"));
-    }
-    catch (Exception e)
-    {
-      return null;
+      try {
+        return new ByteArrayInputStream(buffer.toString().getBytes("UTF8"));
+      } catch (UnsupportedEncodingException e) {
+        logger.error(e);
+        return null;
+      }
     }
   }
-}
+
 
 class ListOfFiles implements Enumeration<FileInputStream>
 {
