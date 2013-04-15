174035940149b5be874f4a4c869c23471e261796
Luis Silva <luis.silva@webdetails.pt>
2013-04-15 12:00:07 +0100

    CDE can now read styles and components from other sources

--- server/plugin/src/pt/webdetails/cdf/dd/CdfStyles.java
+++ server/plugin/src/pt/webdetails/cdf/dd/CdfStyles.java
@@ -6,7 +6,6 @@ package pt.webdetails.cdf.dd;
 
 import net.sf.json.JSONArray;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IParameterProvider;
@@ -18,9 +17,14 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import pt.webdetails.cpf.plugins.PluginsAnalyzer;
+import pt.webdetails.cpf.plugins.Plugin;
+
 
 @SuppressWarnings("unchecked")
 public class CdfStyles {
@@ -29,8 +33,8 @@ public class CdfStyles {
 
   private static CdfStyles instance;
   
-  private static String RESOURCE_STYLES_DIR = "resources/styles";
-  private static String RESOURCE_STYLES_DIR_SOLUTION = "styles";
+  private static final String RESOURCE_STYLES_DIR = "resources/styles/";
+  private static final String RESOURCE_STYLES_DIR_SOLUTION = "styles/";
   
   public static final String DEFAULTSTYLE = "Clean";
 
@@ -72,50 +76,117 @@ public class CdfStyles {
 
   }
 
+  private class Style{
+    String pluginId = null;
+    File directory = null;
+    List<File> styleFiles = null;
+    
+    public Style(File directory, String pluginName){
+      
+      this.directory = directory;
+      this.pluginId = pluginName;
+      styleSelfBuild();
+    }
+    
+    private void styleSelfBuild(){
+      styleFiles = new ArrayList<File>();
+      final FilenameFilter htmlFilter = new FilenameFilter() {
+        public boolean accept(final File dir, final String name) {
+          return name.endsWith(".html");
+        }
+      };
+      
+      if(getDirectory().isDirectory()){
+        String files[] = directory.list(htmlFilter);
+        
+        for(String file : files){
+          File f = new File(directory.getAbsolutePath() + "/" + file);
+          styleFiles.add(f);
+        }
+        
+      }
+      
+    }
+
+    public File getDirectory()
+    {
+      return directory;
+    }
+
+    public String getPluginId()
+    {
+      return pluginId;
+    }
+    
+    public String getSufixPluginName(){
+      String sufix = getPluginId();
+      
+      if(sufix == null){
+        sufix = "";
+      }else{
+        sufix = " - ("+getPluginId()+")";
+      }
+      
+      return sufix;
+    }
+
+
+    public List<File> getStyleFiles()
+    {
+      return styleFiles;
+    }
+    
+  }
 
   public Object liststyles(HashMap<String,String> parameters) throws DashboardDesignerException {
 
     JSONArray result = new JSONArray();
 
-
-    final File defaultStylesDir = new File(ResourceManager.PLUGIN_DIR + RESOURCE_STYLES_DIR);
-    final File customStylesDir = new File(ResourceManager.SOLUTION_DIR + RESOURCE_STYLES_DIR_SOLUTION);
+    List<Style> styles = new ArrayList<Style>();
+    Style style = null; 
+    style = new Style(new File(ResourceManager.PLUGIN_DIR + RESOURCE_STYLES_DIR), null);
+    styles.add(style);
+    style = new Style(new File(ResourceManager.SOLUTION_DIR + RESOURCE_STYLES_DIR_SOLUTION), null);
+    styles.add(style);
     
-    final FilenameFilter htmlFilter = new FilenameFilter() {
-      public boolean accept(final File dir, final String name) {
-        return name.endsWith(".html");
+    PluginsAnalyzer pluginsAnalyzer = new PluginsAnalyzer();
+    
+    
+    List<PluginsAnalyzer.PluginWithEntity> entities = pluginsAnalyzer.getRegisteredEntities("/cde-styles");
+    
+    for(PluginsAnalyzer.PluginWithEntity entity : entities){
+      String solutionPath = PentahoSystem.getApplicationContext().getSolutionPath("");
+      String pluginStylesDir = entity.getRegisteredEntity().valueOf("path");
+      String finalPath = solutionPath+pluginStylesDir+"/";
+      String pluginId = entity.getPlugin().getId().toUpperCase();
+      style = null;
+      
+      File folder = new File(finalPath);
+      
+      if(folder.isDirectory()){
+        style = new Style(folder, pluginId);
+        styles.add(style);
+        
       }
-    };
+    }
+    
+    
 
-    //get bundled styles
-    File[] styleFiles = defaultStylesDir.listFiles(htmlFilter);
+    
 
-    if (styleFiles == null) {
+    if (styles == null || styles.size() < 1) {
       logger.error("No styles directory found in resources");
-      styleFiles = new File[0];
+      styles = new ArrayList<Style>();
     }
+    
 
-    Arrays.sort(styleFiles);
-
-    for (File styleFile : styleFiles) {
-      final String name = styleFile.getName();
-      result.add(name.substring(0,name.lastIndexOf('.')));
-    }
-   
-    //append custom styles
-    if(customStylesDir.isDirectory()){
-      File[] customStyleFiles = customStylesDir.listFiles(htmlFilter);
-      Arrays.sort(customStyleFiles);
-      for (File styleFile : customStyleFiles) 
-      {
-        final String name = styleFile.getName();
-        String styleEntry = name.substring(0,name.lastIndexOf('.'));
-        if(!ArrayUtils.contains(styleFiles, styleEntry))
-        {
-          result.add(styleEntry);
-        }
-        else logger.error("Custom style " + styleEntry + " has same name as another style in resources.");
-      } 
+    for (Style s : styles) {
+      List<File> styleFiles = s.getStyleFiles();
+      
+      for(File file : styleFiles){
+        String name = file.getName();
+        result.add(name.substring(0,name.lastIndexOf('.')) + s.getSufixPluginName());
+      }
     }
 
     return result;
@@ -133,9 +204,36 @@ public class CdfStyles {
   }
 
   public String getResourceLocation(String style) {
-    String customStylePath = RESOURCE_STYLES_DIR_SOLUTION + "/" + style + ".html";
-    File styleFile = new File(ResourceManager.SOLUTION_DIR + customStylePath);
-    if(styleFile.exists()) return customStylePath;
-    else return RESOURCE_STYLES_DIR + "/" + style + ".html";
+    
+    String stylePath = null;
+    String styleFilename = null;
+    
+    String [] split = style.split(" - ");
+    if(split.length > 1){
+      String pluginId = split[1].replace("(", "").replace(")", "");
+      styleFilename = split[0] + ".html";
+      
+      PluginsAnalyzer pluginsAnalizer = new PluginsAnalyzer();
+      
+      List<Plugin> plugins = pluginsAnalizer.getInstalledPlugins();
+      
+      for(Plugin plugin : plugins){
+        if(plugin.getId().equalsIgnoreCase(pluginId)){
+          stylePath = "/" + plugin.getRegisteredEntities("/cde-styles").valueOf("path")+ "/" + styleFilename;
+          break;
+        }
+      }
+    }else{
+      styleFilename = style+".html";
+      String customStylePath = RESOURCE_STYLES_DIR_SOLUTION + styleFilename;
+      File styleFile = new File(ResourceManager.SOLUTION_DIR + customStylePath);
+      if(styleFile.exists()){ 
+        stylePath =  customStylePath;
+      }else if( new File(ResourceManager.PLUGIN_DIR + RESOURCE_STYLES_DIR+"/"+styleFilename).exists()){ 
+        stylePath = RESOURCE_STYLES_DIR + styleFilename;
+      }
+    }
+    
+    return stylePath;
   }
 }
