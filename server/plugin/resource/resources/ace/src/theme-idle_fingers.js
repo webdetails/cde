9b41e5c52bbaa9412a83acb8140910e1fda25f77
Luis Silva <luis.silva@webdetails.pt>
2013-05-14 14:16:59 +0100

    Updated Ace editor version to 1.0

--- server/plugin/resource/resources/ace/src/theme-idle_fingers.js
+++ server/plugin/resource/resources/ace/src/theme-idle_fingers.js
@@ -1,1 +1,136 @@
-define("ace/theme/idle_fingers",["require","exports","module"],function(a,b,c){var d=a("pilot/dom"),e=".ace-idle-fingers .ace_editor {\n  border: 2px solid rgb(159, 159, 159);\n}\n\n.ace-idle-fingers .ace_editor.ace_focus {\n  border: 2px solid #327fbd;\n}\n\n.ace-idle-fingers .ace_gutter {\n  width: 50px;\n  background: #e8e8e8;\n  color: #333;\n  overflow : hidden;\n}\n\n.ace-idle-fingers .ace_gutter-layer {\n  width: 100%;\n  text-align: right;\n}\n\n.ace-idle-fingers .ace_gutter-layer .ace_gutter-cell {\n  padding-right: 6px;\n}\n\n.ace-idle-fingers .ace_print_margin {\n  width: 1px;\n  background: #e8e8e8;\n}\n\n.ace-idle-fingers .ace_scroller {\n  background-color: #323232;\n}\n\n.ace-idle-fingers .ace_text-layer {\n  cursor: text;\n  color: #FFFFFF;\n}\n\n.ace-idle-fingers .ace_cursor {\n  border-left: 2px solid #91FF00;\n}\n\n.ace-idle-fingers .ace_cursor.ace_overwrite {\n  border-left: 0px;\n  border-bottom: 1px solid #91FF00;\n}\n \n.ace-idle-fingers .ace_marker-layer .ace_selection {\n  background: rgba(90, 100, 126, 0.88);\n}\n\n.ace-idle-fingers .ace_marker-layer .ace_step {\n  background: rgb(198, 219, 174);\n}\n\n.ace-idle-fingers .ace_marker-layer .ace_bracket {\n  margin: -1px 0 0 -1px;\n  border: 1px solid #404040;\n}\n\n.ace-idle-fingers .ace_marker-layer .ace_active_line {\n  background: #353637;\n}\n\n       \n.ace-idle-fingers .ace_invisible {\n  color: #404040;\n}\n\n.ace-idle-fingers .ace_keyword {\n  color:#CC7833;\n}\n\n.ace-idle-fingers .ace_keyword.ace_operator {\n  \n}\n\n.ace-idle-fingers .ace_constant {\n  color:#6C99BB;\n}\n\n.ace-idle-fingers .ace_constant.ace_language {\n  \n}\n\n.ace-idle-fingers .ace_constant.ace_library {\n  \n}\n\n.ace-idle-fingers .ace_constant.ace_numeric {\n  \n}\n\n.ace-idle-fingers .ace_invalid {\n  color:#FFFFFF;\nbackground-color:#FF0000;\n}\n\n.ace-idle-fingers .ace_invalid.ace_illegal {\n  \n}\n\n.ace-idle-fingers .ace_invalid.ace_deprecated {\n  \n}\n\n.ace-idle-fingers .ace_support {\n  \n}\n\n.ace-idle-fingers .ace_support.ace_function {\n  color:#B83426;\n}\n\n.ace-idle-fingers .ace_function.ace_buildin {\n  \n}\n\n.ace-idle-fingers .ace_string {\n  color:#A5C261;\n}\n\n.ace-idle-fingers .ace_string.ace_regexp {\n  color:#CCCC33;\n}\n\n.ace-idle-fingers .ace_comment {\n  font-style:italic;\ncolor:#BC9458;\n}\n\n.ace-idle-fingers .ace_comment.ace_doc {\n  \n}\n\n.ace-idle-fingers .ace_comment.ace_doc.ace_tag {\n  \n}\n\n.ace-idle-fingers .ace_variable {\n  \n}\n\n.ace-idle-fingers .ace_variable.ace_language {\n  \n}\n\n.ace-idle-fingers .ace_xml_pe {\n  \n}\n\n.ace-idle-fingers .ace_meta {\n  \n}\n\n.ace-idle-fingers .ace_meta.ace_tag {\n  color:#FFE5BB;\n}\n\n.ace-idle-fingers .ace_meta.ace_tag.ace_input {\n  \n}\n\n.ace-idle-fingers .ace_entity.ace_other.ace_attribute-name {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_underline {\n    text-decoration:underline;\n}\n\n.ace-idle-fingers .ace_markup.ace_heading {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_heading.ace_1 {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_heading.ace_2 {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_heading.ace_3 {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_heading.ace_4 {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_heading.ace_5 {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_heading.ace_6 {\n  \n}\n\n.ace-idle-fingers .ace_markup.ace_list {\n  \n}\n\n.ace-idle-fingers .ace_collab.ace_user1 {\n  color:#323232;\nbackground-color:#FFF980;   \n}";d.importCssString(e),b.cssClass="ace-idle-fingers"})
+/* ***** BEGIN LICENSE BLOCK *****
+ * Distributed under the BSD license:
+ *
+ * Copyright (c) 2010, Ajax.org B.V.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Ajax.org B.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+define('ace/theme/idle_fingers', ['require', 'exports', 'module' , 'ace/lib/dom'], function(require, exports, module) {
+
+exports.isDark = true;
+exports.cssClass = "ace-idle-fingers";
+exports.cssText = ".ace-idle-fingers .ace_gutter {\
+background: #3b3b3b;\
+color: #fff\
+}\
+.ace-idle-fingers .ace_print-margin {\
+width: 1px;\
+background: #3b3b3b\
+}\
+.ace-idle-fingers .ace_scroller {\
+background-color: #323232\
+}\
+.ace-idle-fingers .ace_text-layer {\
+color: #FFFFFF\
+}\
+.ace-idle-fingers .ace_cursor {\
+border-left: 2px solid #91FF00\
+}\
+.ace-idle-fingers .ace_overwrite-cursors .ace_cursor {\
+border-left: 0px;\
+border-bottom: 1px solid #91FF00\
+}\
+.ace-idle-fingers .ace_marker-layer .ace_selection {\
+background: rgba(90, 100, 126, 0.88)\
+}\
+.ace-idle-fingers.ace_multiselect .ace_selection.ace_start {\
+box-shadow: 0 0 3px 0px #323232;\
+border-radius: 2px\
+}\
+.ace-idle-fingers .ace_marker-layer .ace_step {\
+background: rgb(102, 82, 0)\
+}\
+.ace-idle-fingers .ace_marker-layer .ace_bracket {\
+margin: -1px 0 0 -1px;\
+border: 1px solid #404040\
+}\
+.ace-idle-fingers .ace_marker-layer .ace_active-line {\
+background: #353637\
+}\
+.ace-idle-fingers .ace_gutter-active-line {\
+background-color: #353637\
+}\
+.ace-idle-fingers .ace_marker-layer .ace_selected-word {\
+border: 1px solid rgba(90, 100, 126, 0.88)\
+}\
+.ace-idle-fingers .ace_invisible {\
+color: #404040\
+}\
+.ace-idle-fingers .ace_keyword,\
+.ace-idle-fingers .ace_meta {\
+color: #CC7833\
+}\
+.ace-idle-fingers .ace_constant,\
+.ace-idle-fingers .ace_constant.ace_character,\
+.ace-idle-fingers .ace_constant.ace_character.ace_escape,\
+.ace-idle-fingers .ace_constant.ace_other,\
+.ace-idle-fingers .ace_support.ace_constant {\
+color: #6C99BB\
+}\
+.ace-idle-fingers .ace_invalid {\
+color: #FFFFFF;\
+background-color: #FF0000\
+}\
+.ace-idle-fingers .ace_fold {\
+background-color: #CC7833;\
+border-color: #FFFFFF\
+}\
+.ace-idle-fingers .ace_support.ace_function {\
+color: #B83426\
+}\
+.ace-idle-fingers .ace_variable.ace_parameter {\
+font-style: italic\
+}\
+.ace-idle-fingers .ace_string {\
+color: #A5C261\
+}\
+.ace-idle-fingers .ace_string.ace_regexp {\
+color: #CCCC33\
+}\
+.ace-idle-fingers .ace_comment {\
+font-style: italic;\
+color: #BC9458\
+}\
+.ace-idle-fingers .ace_meta.ace_tag {\
+color: #FFE5BB\
+}\
+.ace-idle-fingers .ace_entity.ace_name {\
+color: #FFC66D\
+}\
+.ace-idle-fingers .ace_markup.ace_underline {\
+text-decoration: underline\
+}\
+.ace-idle-fingers .ace_collab.ace_user1 {\
+color: #323232;\
+background-color: #FFF980\
+}\
+.ace-idle-fingers .ace_indent-guide {\
+background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAEklEQVQImWMwMjL6zzBz5sz/ABEUBGCqhK6UAAAAAElFTkSuQmCC) right repeat-y\
+}";
+
+var dom = require("../lib/dom");
+dom.importCssString(exports.cssText, exports.cssClass);
+});
--- server/plugin/resource/resources/ace/src/theme-solarized_light.js
+++ server/plugin/resource/resources/ace/src/theme-solarized_light.js
@@ -1,1 +1,131 @@
-define("ace/theme/solarized_light",["require","exports","module"],function(a,b,c){var d=a("pilot/dom"),e=".ace-solarized-light .ace_editor {\n  border: 2px solid rgb(159, 159, 159);\n}\n\n.ace-solarized-light .ace_editor.ace_focus {\n  border: 2px solid #327fbd;\n}\n\n.ace-solarized-light .ace_gutter {\n  width: 50px;\n  background: #e8e8e8;\n  color: #333;\n  overflow : hidden;\n}\n\n.ace-solarized-light .ace_gutter-layer {\n  width: 100%;\n  text-align: right;\n}\n\n.ace-solarized-light .ace_gutter-layer .ace_gutter-cell {\n  padding-right: 6px;\n}\n\n.ace-solarized-light .ace_print_margin {\n  width: 1px;\n  background: #e8e8e8;\n}\n\n.ace-solarized-light .ace_scroller {\n  background-color: #FDF6E3;\n}\n\n.ace-solarized-light .ace_text-layer {\n  cursor: text;\n  color: #586E75;\n}\n\n.ace-solarized-light .ace_cursor {\n  border-left: 2px solid #000000;\n}\n\n.ace-solarized-light .ace_cursor.ace_overwrite {\n  border-left: 0px;\n  border-bottom: 1px solid #000000;\n}\n \n.ace-solarized-light .ace_marker-layer .ace_selection {\n  background: #073642;\n}\n\n.ace-solarized-light .ace_marker-layer .ace_step {\n  background: rgb(198, 219, 174);\n}\n\n.ace-solarized-light .ace_marker-layer .ace_bracket {\n  margin: -1px 0 0 -1px;\n  border: 1px solid rgba(147, 161, 161, 0.50);\n}\n\n.ace-solarized-light .ace_marker-layer .ace_active_line {\n  background: #EEE8D5;\n}\n\n       \n.ace-solarized-light .ace_invisible {\n  color: rgba(147, 161, 161, 0.50);\n}\n\n.ace-solarized-light .ace_keyword {\n  color:#859900;\n}\n\n.ace-solarized-light .ace_keyword.ace_operator {\n  \n}\n\n.ace-solarized-light .ace_constant {\n  \n}\n\n.ace-solarized-light .ace_constant.ace_language {\n  color:#B58900;\n}\n\n.ace-solarized-light .ace_constant.ace_library {\n  \n}\n\n.ace-solarized-light .ace_constant.ace_numeric {\n  color:#D33682;\n}\n\n.ace-solarized-light .ace_invalid {\n  \n}\n\n.ace-solarized-light .ace_invalid.ace_illegal {\n  \n}\n\n.ace-solarized-light .ace_invalid.ace_deprecated {\n  \n}\n\n.ace-solarized-light .ace_support {\n  \n}\n\n.ace-solarized-light .ace_support.ace_function {\n  color:#268BD2;\n}\n\n.ace-solarized-light .ace_function.ace_buildin {\n  \n}\n\n.ace-solarized-light .ace_string {\n  color:#2AA198;\n}\n\n.ace-solarized-light .ace_string.ace_regexp {\n  color:#D30102;\n}\n\n.ace-solarized-light .ace_comment {\n  color:#93A1A1;\n}\n\n.ace-solarized-light .ace_comment.ace_doc {\n  \n}\n\n.ace-solarized-light .ace_comment.ace_doc.ace_tag {\n  \n}\n\n.ace-solarized-light .ace_variable {\n  \n}\n\n.ace-solarized-light .ace_variable.ace_language {\n  color:#268BD2;\n}\n\n.ace-solarized-light .ace_xml_pe {\n  \n}\n\n.ace-solarized-light .ace_meta {\n  \n}\n\n.ace-solarized-light .ace_meta.ace_tag {\n  \n}\n\n.ace-solarized-light .ace_meta.ace_tag.ace_input {\n  \n}\n\n.ace-solarized-light .ace_entity.ace_other.ace_attribute-name {\n  color:#93A1A1;\n}\n\n.ace-solarized-light .ace_markup.ace_underline {\n    text-decoration:underline;\n}\n\n.ace-solarized-light .ace_markup.ace_heading {\n  \n}\n\n.ace-solarized-light .ace_markup.ace_heading.ace_1 {\n  \n}\n\n.ace-solarized-light .ace_markup.ace_heading.ace_2 {\n  \n}\n\n.ace-solarized-light .ace_markup.ace_heading.ace_3 {\n  \n}\n\n.ace-solarized-light .ace_markup.ace_heading.ace_4 {\n  \n}\n\n.ace-solarized-light .ace_markup.ace_heading.ace_5 {\n  \n}\n\n.ace-solarized-light .ace_markup.ace_heading.ace_6 {\n  \n}\n\n.ace-solarized-light .ace_markup.ace_list {\n  \n}\n\n.ace-solarized-light .ace_collab.ace_user1 {\n     \n}";d.importCssString(e),b.cssClass="ace-solarized-light"})
+/* ***** BEGIN LICENSE BLOCK *****
+ * Distributed under the BSD license:
+ *
+ * Copyright (c) 2010, Ajax.org B.V.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Ajax.org B.V. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+define('ace/theme/solarized_light', ['require', 'exports', 'module' , 'ace/lib/dom'], function(require, exports, module) {
+
+exports.isDark = false;
+exports.cssClass = "ace-solarized-light";
+exports.cssText = ".ace-solarized-light .ace_gutter {\
+background: #fbf1d3;\
+color: #333\
+}\
+.ace-solarized-light .ace_print-margin {\
+width: 1px;\
+background: #e8e8e8\
+}\
+.ace-solarized-light .ace_scroller {\
+background-color: #FDF6E3\
+}\
+.ace-solarized-light .ace_text-layer {\
+color: #586E75\
+}\
+.ace-solarized-light .ace_cursor {\
+border-left: 2px solid #000000\
+}\
+.ace-solarized-light .ace_overwrite-cursors .ace_cursor {\
+border-left: 0px;\
+border-bottom: 1px solid #000000\
+}\
+.ace-solarized-light .ace_marker-layer .ace_selection {\
+background: #073642\
+}\
+.ace-solarized-light.ace_multiselect .ace_selection.ace_start {\
+box-shadow: 0 0 3px 0px #FDF6E3;\
+border-radius: 2px\
+}\
+.ace-solarized-light .ace_marker-layer .ace_step {\
+background: rgb(255, 255, 0)\
+}\
+.ace-solarized-light .ace_marker-layer .ace_bracket {\
+margin: -1px 0 0 -1px;\
+border: 1px solid rgba(147, 161, 161, 0.50)\
+}\
+.ace-solarized-light .ace_marker-layer .ace_active-line {\
+background: #EEE8D5\
+}\
+.ace-solarized-light .ace_gutter-active-line {\
+background-color : #dcdcdc\
+}\
+.ace-solarized-light .ace_marker-layer .ace_selected-word {\
+border: 1px solid #073642\
+}\
+.ace-solarized-light .ace_invisible {\
+color: rgba(147, 161, 161, 0.50)\
+}\
+.ace-solarized-light .ace_keyword,\
+.ace-solarized-light .ace_meta,\
+.ace-solarized-light .ace_support.ace_class,\
+.ace-solarized-light .ace_support.ace_type {\
+color: #859900\
+}\
+.ace-solarized-light .ace_constant.ace_character,\
+.ace-solarized-light .ace_constant.ace_other {\
+color: #CB4B16\
+}\
+.ace-solarized-light .ace_constant.ace_language {\
+color: #B58900\
+}\
+.ace-solarized-light .ace_constant.ace_numeric {\
+color: #D33682\
+}\
+.ace-solarized-light .ace_fold {\
+background-color: #268BD2;\
+border-color: #586E75\
+}\
+.ace-solarized-light .ace_entity.ace_name.ace_function,\
+.ace-solarized-light .ace_entity.ace_name.ace_tag,\
+.ace-solarized-light .ace_support.ace_function,\
+.ace-solarized-light .ace_variable,\
+.ace-solarized-light .ace_variable.ace_language {\
+color: #268BD2\
+}\
+.ace-solarized-light .ace_storage {\
+color: #073642\
+}\
+.ace-solarized-light .ace_string {\
+color: #2AA198\
+}\
+.ace-solarized-light .ace_string.ace_regexp {\
+color: #D30102\
+}\
+.ace-solarized-light .ace_comment,\
+.ace-solarized-light .ace_entity.ace_other.ace_attribute-name {\
+color: #93A1A1\
+}\
+.ace-solarized-light .ace_markup.ace_underline {\
+text-decoration: underline\
+}\
+.ace-solarized-light .ace_indent-guide {\
+background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4++3xf4ZVq1b9BwAjxwbT1g3hiwAAAABJRU5ErkJggg==) right repeat-y\
+}";
+
+var dom = require("../lib/dom");
+dom.importCssString(exports.cssText, exports.cssClass);
+});
