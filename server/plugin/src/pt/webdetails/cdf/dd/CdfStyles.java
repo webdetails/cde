151ac36d6fc2ff924da286f543c794108b901c5c
Luis Silva <luis.silva@webdetails.pt>
2013-05-27 09:35:49 +0100

    pluginsAnalizer on CdfStyles wasn't being refreshed before being used. Fixed

--- server/plugin/src/pt/webdetails/cdf/dd/CdfStyles.java
+++ server/plugin/src/pt/webdetails/cdf/dd/CdfStyles.java
@@ -215,6 +215,7 @@ public class CdfStyles {
       styleFilename = split[0] + ".html";
       
       PluginsAnalyzer pluginsAnalizer = new PluginsAnalyzer();
+      pluginsAnalizer.refresh();
       
       List<Plugin> plugins = pluginsAnalizer.getInstalledPlugins();
       
