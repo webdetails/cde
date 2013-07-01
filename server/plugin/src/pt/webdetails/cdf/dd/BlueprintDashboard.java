9f879eac9e7872c8ebd3f6fe874fa74a46d44c82
Rafael Gomes <rafael.gomes@webdetails.pt>
2013-07-01 11:16:45 +0100

    fixes relative path construction of url for js and css resources

--- server/plugin/src/pt/webdetails/cdf/dd/BlueprintDashboard.java
+++ server/plugin/src/pt/webdetails/cdf/dd/BlueprintDashboard.java
@@ -16,99 +16,85 @@ import pt.webdetails.cdf.dd.structure.WcdfDescriptor;
  *
  * @author pdpi
  */
-public class BlueprintDashboard extends AbstractDashboard
-{
+public class BlueprintDashboard extends AbstractDashboard {
   /* CONSTANTS */
 
   private static final long serialVersionUID = 1L;
+
   private static Log logger = LogFactory.getLog(Dashboard.class);
+
   protected final static String TYPE = "blueprint";
 
-  public BlueprintDashboard(IParameterProvider pathParams, IParameterProvider requestParams) throws FileNotFoundException
-  {
+  public BlueprintDashboard(IParameterProvider pathParams, IParameterProvider requestParams)
+      throws FileNotFoundException {
     super(pathParams, requestParams);
   }
 
-  public BlueprintDashboard(WcdfDescriptor wcdf, boolean absolute, String absRoot, boolean debug, String scheme, String alias) throws FileNotFoundException
-  {
+  public BlueprintDashboard(WcdfDescriptor wcdf, boolean absolute, String absRoot, boolean debug, String scheme,
+      String alias) throws FileNotFoundException {
     super(wcdf, absolute, absRoot, debug, scheme, alias);
   }
 
-  public BlueprintDashboard(WcdfDescriptor wcdf, boolean absolute, String absRoot, boolean debug, String scheme) throws FileNotFoundException
-  {
+  public BlueprintDashboard(WcdfDescriptor wcdf, boolean absolute, String absRoot, boolean debug, String scheme)
+      throws FileNotFoundException {
     super(wcdf, absolute, absRoot, debug, scheme);
   }
 
-  public BlueprintDashboard(String wcdfPath, boolean absolute, String absRoot, boolean debug, String scheme) throws FileNotFoundException
-  {
+  public BlueprintDashboard(String wcdfPath, boolean absolute, String absRoot, boolean debug, String scheme)
+      throws FileNotFoundException {
     super(wcdfPath, absolute, absRoot, debug, scheme);
   }
 
-  protected String renderHeaders(String contents)
-  {
+  protected String renderHeaders(String contents) {
     String dependencies, styles, cdfDependencies;
     final String title = "<title>" + getWcdf().getTitle() + "</title>";
+    
     // Acquire CDF headers
-    try
-    {
+    try {
       cdfDependencies = DashboardDesignerContentGenerator.getCdfIncludes(contents, getType(), debug, absRoot, scheme);
-    }
-    catch (Exception e)
-    {
+    } catch (Exception e) {
       logger.error("Failed to get cdf includes");
       cdfDependencies = "";
     }
-    // Acquire CDE-specific headers
-    if (absolute)
-    {
-      final String adornedRoot = (scheme.equals("") ? "" : (scheme + "://")) + absRoot;
-      StringFilter css = new StringFilter()
-      {
-        public String filter(String input)
-        {
-          //input = input.replaceAll("\\?", "&");
-          return "\t\t<link href='" + adornedRoot + DashboardDesignerContentGenerator.SERVER_URL_VALUE + "getCssResource/" + input + "' rel='stylesheet' type='text/css' />\n";
-        }
-      };
-      StringFilter js = new StringFilter()
-      {
-        public String filter(String input)
-        {
-          //input = input.replaceAll("\\?", "&");
-          return "\t\t<script language=\"javascript\" type=\"text/javascript\" src=\"" + adornedRoot + DashboardDesignerContentGenerator.SERVER_URL_VALUE + "getJsResource/" + input + "\"></script>\n";
-        }
-      };
-      if (debug)
-      {
-        dependencies = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF).getDependencies(js);
-        styles = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF_CSS).getDependencies(css);
+
+    final String adornedRoot = absolute ? ((scheme.equals("") ? "" : (scheme + "://")) + absRoot) : "";
+
+    StringFilter js = new StringFilter() {
+
+      @Override
+      public String filter(String input) {
+        return String.format(
+            "\t\t<script language=\"javascript\" type=\"text/javascript\" src=\"%s%s%s%s\"></script>\n", adornedRoot,
+            DashboardDesignerContentGenerator.SERVER_URL_VALUE, "getJsResource/", input);
       }
-      else
-      {
-        dependencies = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF).getPackagedDependencies(js);
-        styles = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF_CSS).getPackagedDependencies(css);
+
+    };
+
+    StringFilter css = new StringFilter() {
+      @Override
+      public String filter(String input) {
+        return String.format(
+            "\t\t<link rel=\"stylesheet\" href=\"%s%s%s%s\"></script>\n", adornedRoot,
+            DashboardDesignerContentGenerator.SERVER_URL_VALUE, "getCssResource/", input);
       }
-    }
-    else
-    {
-      if (debug)
-      {
-        dependencies = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF).getDependencies();
-        styles = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF_CSS).getDependencies();
-      }
-      else
-      {
-        dependencies = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF).getPackagedDependencies();
-        styles = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF_CSS).getPackagedDependencies();
-      }
+
+    };
+
+    if (debug) {
+      dependencies = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF).getDependencies(js);
+      styles = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF_CSS).getDependencies(css);
+    } else {
+      dependencies = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF)
+          .getPackagedDependencies(js);
+      styles = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF_CSS)
+          .getPackagedDependencies(css);
     }
 
     String raw = DependenciesManager.getInstance().getEngine(DependenciesManager.Engines.CDF_RAW).getDependencies();
     return title + cdfDependencies + raw + dependencies + styles;
   }
 
-  public String getType()
-  {
+  public String getType() {
     return TYPE;
   }
 }
