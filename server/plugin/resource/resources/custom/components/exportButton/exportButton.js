161287b8c577b896b6f8cc6d16c0eab889ec8c9d
Goncalo Neto <goncalo.neto@webdetails.pt>
2013-07-23 17:47:10 +0100

    Remove class test from exportButton. Conflicts: 	server/plugin/resource/resources/custom/components/exportButton/exportButton.js

--- server/plugin/resource/resources/custom/components/exportButton/exportButton.js
+++ server/plugin/resource/resources/custom/components/exportButton/exportButton.js
@@ -19,9 +19,45 @@ var ExportButtonComponent = BaseComponent.extend({
 	var componentName = (myself.componentName.indexOf("render_")==0?"":"render_") + myself.componentName;
     var comp = Dashboards.getComponentByName( componentName );
     var overrideParameters = Dashboards.propertiesArrayToObject(myself.parameters);    
-    bar.text( this.label ).click( function(){
-      component.query.exportData(myself.outputType, overrideParameters);
-    } );
+    bar.text( myself.label ).click( function(){
+      var foundQuery = false;
+      for (n in myself.queryObjectNames) {
+        if(comp[n]){
+            comp[n].exportData(myself.outputType, overrideParameters , myself.getFilterSettings(comp) );
+            foundQuery = true;
+            break;
+        }
+      }
+      if (!foundQuery) { 
+        Dashboards.log( myself.name + ": could not find a query object on " + myself.compName ); 
+      }
+    });
+  },
+  
+  //add filtering to export if it's a data table
+  getFilterSettings: function(component){
+    var extraSettings = {};
+	if(component.type == "Table"){
+	  //var dtOptions = component.getTableOptions(component.chartDefinition);
+	  //if(dtOptions.bFilter) {
+	  var dtOptions = component.ph.dataTableSettings[0];
+	  if(dtOptions.oFeatures.bFilter) {
+		var searchInput = component.ph.find('input');
+		if(searchInput) {
+		  extraSettings.dtFilter = searchInput.val();
+		  if(dtOptions.aoColumns){
+			var idxs = [];
+		    for(var i=0; i< dtOptions.aoColumns.length;i++){
+			  if(dtOptions.aoColumns[i].bSearchable){
+			    idxs.push(i);
+		      }
+			}
+			extraSettings.dtSearchableColumns = idxs.join(',');
+		  }
+		}
+	  }
+	}
+	return extraSettings;
   }
 
 });
