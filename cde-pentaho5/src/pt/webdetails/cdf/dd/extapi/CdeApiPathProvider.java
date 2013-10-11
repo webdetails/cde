27596ba67a81c51e09b8612de7778a8b0841c956
tgf <tiago.n.ferreira@gmail.com>
2013-10-11 15:38:25 +0100

    missing commit

---
+++ cde-pentaho5/src/pt/webdetails/cdf/dd/extapi/CdeApiPathProvider.java
@@ -0,0 +1,19 @@
+package pt.webdetails.cdf.dd.extapi;
+
+import pt.webdetails.cpf.Util;
+import pt.webdetails.cpf.context.api.IUrlProvider;
+
+public class CdeApiPathProvider implements ICdeApiPathProvider {
+
+  private IUrlProvider urlProvider;
+
+  public CdeApiPathProvider( IUrlProvider urlProvider ) {
+    this.urlProvider = urlProvider;
+  }
+
+  @Override
+  public String getRendererBasePath() {
+    return Util.joinPath( urlProvider.getPluginBaseUrl(), "renderer" );
+  }
+
+}
