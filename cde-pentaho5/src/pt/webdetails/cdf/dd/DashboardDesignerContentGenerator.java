d587a0349b6362fa2002d29e965310321653c0ea
Pedro Teixeira <pedrofvteixeira@gmail.com>
2013-10-14 15:54:24 +0100

    fixed res calls

--- cde-pentaho5/src/pt/webdetails/cdf/dd/DashboardDesignerContentGenerator.java
+++ cde-pentaho5/src/pt/webdetails/cdf/dd/DashboardDesignerContentGenerator.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IParameterProvider;
 
 import pt.webdetails.cdf.dd.api.RenderApi;
+import pt.webdetails.cdf.dd.api.ResourcesApi;
 import pt.webdetails.cdf.dd.util.CdeEnvironment;
 import pt.webdetails.cpf.SimpleContentGenerator;
 
@@ -19,6 +20,7 @@ public class DashboardDesignerContentGenerator extends SimpleContentGenerator {
 
   private boolean edit = false;
   private boolean create = false;
+  private boolean resource = false;
 
   public DashboardDesignerContentGenerator() {
     super();
@@ -58,6 +60,15 @@ public class DashboardDesignerContentGenerator extends SimpleContentGenerator {
     } else if( edit ) {
     	renderer.edit( "", "", filePath, debug, getRequest(), getResponse() );
     
+    } else if( resource ) {
+    	// TODO review later if there is a viable solution to making resources being 
+    	// called via cde resources rest api (pentaho/plugin/pentaho-cdf-dd/api/resources?resource=)
+    	// this has to take into consideration:
+    	// 1 - token replacement (see cde-core#CdfRunJsDashboardWriteContext.replaceTokens())
+    	// 2 - resources being called from other resources (ex: resource plugin-samples/template.css calls resource images/button-contact-png) 
+    	
+    	new ResourcesApi().getResource(pathParams.getStringParameter( MethodParams.COMMAND, "" ), getResponse());
+    
     } else {
     	String result = renderer.render( "", "", filePath, inferScheme, root, absolute, bypassCacheRead, debug, viewId, getRequest());
 
@@ -82,6 +93,7 @@ public class DashboardDesignerContentGenerator extends SimpleContentGenerator {
     public static final String FILE = "file";
     public static final String REQUEST = "request";
     public static final String VIEWID = "viewId";
+    public static final String COMMAND = "cmd";
 
     public static final String DATA = "data";
   }
@@ -100,5 +112,13 @@ public class DashboardDesignerContentGenerator extends SimpleContentGenerator {
 	
 	public void setCreate(boolean create) {
 		this.create = create;
+	}
+
+	public boolean isResource() {
+		return resource;
+	}
+
+	public void setResource(boolean resource) {
+		this.resource = resource;
 	}
 }
