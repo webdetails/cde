5eec57265f016c81d948377d77754884703a863e
Pedro Vale <pedro.vale@webdetails.pt>
2014-02-19 09:52:45 +0000

    [PDB-1555]: Wrong and missing license headers

--- cde-pentaho5/resource/js/widget-cde.js
+++ cde-pentaho5/resource/js/widget-cde.js
@@ -1,11 +1,14 @@
 /*!
-* The Pentaho proprietary code is licensed under the terms and conditions
-* of the software license agreement entered into between the entity licensing
-* such code and Pentaho Corporation.
+* Copyright 2002 - 2014 Webdetails, a Pentaho company. All rights reserved.
 *
-* This software costs money - it is not free
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
 *
-* Copyright 2002 - 2013 Pentaho Corporation.  All rights reserved.
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
+* the license for the specific language governing your rights and limitations.
 */
 
 
