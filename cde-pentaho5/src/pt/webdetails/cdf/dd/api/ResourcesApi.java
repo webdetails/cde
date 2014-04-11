12f30ca25f312faa2879e5274f80eb0bd30273ea
David Santos <david.santos@webdetails.pt>
2014-04-11 10:32:54 +0100

    [CLEANUP]: Reviewed code style and added licence
    Conflicts:
    	cde-pentaho5/src/pt/webdetails/cdf/dd/api/ResourcesApi.java

--- cde-pentaho5/src/pt/webdetails/cdf/dd/api/ResourcesApi.java
+++ cde-pentaho5/src/pt/webdetails/cdf/dd/api/ResourcesApi.java
@@ -1,3 +1,16 @@
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
+
 package pt.webdetails.cdf.dd.api;
 
 import java.io.IOException;
@@ -5,7 +18,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.*;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -85,7 +105,8 @@ public class ResourcesApi {
   @Path( "/getCss" )
   @Produces( "text/css" )
   public void getCssResource( @QueryParam( "path" ) @DefaultValue( "" ) String path,
-      @QueryParam( "resource" ) @DefaultValue( "" ) String resource, @Context HttpServletResponse response )
+                              @QueryParam( "resource" ) @DefaultValue( "" ) String resource,
+                              @Context HttpServletResponse response )
     throws IOException {
     getResource( resource, response );
   }
@@ -94,7 +115,8 @@ public class ResourcesApi {
   @Path( "/getJs" )
   @Produces( "text/javascript" )
   public void getJsResource( @QueryParam( "path" ) @DefaultValue( "" ) String path,
-      @QueryParam( "resource" ) @DefaultValue( "" ) String resource, @Context HttpServletResponse response )
+                             @QueryParam( "resource" ) @DefaultValue( "" ) String resource,
+                             @Context HttpServletResponse response )
     throws IOException {
     getResource( resource, response );
   }
@@ -103,7 +125,8 @@ public class ResourcesApi {
   @Path( "/getUntyped" )
   @Produces( "text/plain" )
   public void getUntypedResource( @QueryParam( "path" ) @DefaultValue( "" ) String path,
-      @QueryParam( "resource" ) @DefaultValue( "" ) String resource, @Context HttpServletResponse response )
+                                  @QueryParam( "resource" ) @DefaultValue( "" ) String resource,
+                                  @Context HttpServletResponse response )
     throws IOException {
     response.setHeader( "content-disposition", "inline" );
 
@@ -114,7 +137,8 @@ public class ResourcesApi {
   @Path( "/getImg" )
   @Produces( "text/plain" )
   public void getImage( @QueryParam( "path" ) @DefaultValue( "" ) String path,
-      @QueryParam( "resource" ) @DefaultValue( "" ) String resource, @Context HttpServletResponse response )
+                        @QueryParam( "resource" ) @DefaultValue( "" ) String resource,
+                        @Context HttpServletResponse response )
     throws IOException {
     getResource( resource, response );
   }
@@ -123,7 +147,8 @@ public class ResourcesApi {
   @Path( "/res" )
   @Produces( "text/plain" )
   public void res( @QueryParam( "path" ) @DefaultValue( "" ) String path,
-      @QueryParam( "resource" ) @DefaultValue( "" ) String resource, @Context HttpServletResponse response )
+                   @QueryParam( "resource" ) @DefaultValue( "" ) String resource,
+                   @Context HttpServletResponse response )
     throws Exception {
     getResource( resource, response );
   }
@@ -131,25 +156,30 @@ public class ResourcesApi {
   @POST
   @Path( "/explore" )
   @Produces( "text/plain" )
-  public String exploreFolder(  @FormParam( "dir" ) @DefaultValue( "/" ) String folder, 
-		  						@FormParam( "outputType" ) String outputType, 
-		  						@QueryParam( "fileExtensions" ) String fileExtensions,
-		  						@QueryParam( "access" ) String access,
-                  @QueryParam( "showHiddenFiles" ) @DefaultValue("false") boolean showHiddenFiles) throws IOException {
+  public String exploreFolder( @FormParam( "dir" ) @DefaultValue( "/" ) String folder,
+                               @FormParam( "outputType" ) String outputType,
+                               @QueryParam( "dashboardPath" ) @DefaultValue( "" ) String dashboardPath,
+                               @QueryParam( "fileExtensions" ) String fileExtensions,
+                               @QueryParam( "access" ) String access,
+                               @QueryParam( "showHiddenFiles" ) @DefaultValue( "false" ) boolean showHiddenFiles )
+    throws IOException {
 
-    if ( !StringUtils.isEmpty(outputType) && outputType.equals( "json" ) ) {
+    if ( !StringUtils.isEmpty( outputType ) && outputType.equals( "json" ) ) {
       try {
-    	 return RepositoryHelper.toJSON( folder, getFileList( folder, fileExtensions, access, showHiddenFiles ) );
+        return RepositoryHelper
+          .toJSON( folder, getFileList( folder, dashboardPath, fileExtensions, access, showHiddenFiles ) );
       } catch ( JSONException e ) {
         logger.error( "exploreFolder" + folder, e );
         return "Error getting files in folder " + folder;
       }
     } else {
-      return RepositoryHelper.toJQueryFileTree( folder, getFileList( folder, fileExtensions, access, showHiddenFiles ) );
+      return RepositoryHelper
+        .toJQueryFileTree( folder, getFileList( folder, dashboardPath, fileExtensions, access, showHiddenFiles ) );
     }
   }
 
-  private IBasicFile[] getFileList( String dir, final String fileExtensions, String permission, boolean showHiddenFiles ) {
+  private IBasicFile[] getFileList( String dir, String dashboardPath, final String fileExtensions, String permission,
+                                    boolean showHiddenFiles ) {
 
     ArrayList<String> extensionsList = new ArrayList<String>();
     String[] extensions = StringUtils.split( fileExtensions, "." );
@@ -168,36 +198,54 @@ public class ResourcesApi {
     }
 
     GenericBasicFileFilter fileFilter =
-        new GenericBasicFileFilter( null, extensionsList.toArray( new String[extensionsList.size()] ), true );
+      new GenericBasicFileFilter( null, extensionsList.toArray( new String[ extensionsList.size() ] ), true );
 
-    List<IBasicFile> fileList =
-        CdeEnvironment.getUserContentAccess().listFiles( dir, fileFilter, IReadAccess.DEPTH_ZERO, true, showHiddenFiles );
-
-    if ( fileList != null && fileList.size() > 0 ) {
-      return fileList.toArray( new IBasicFile[fileList.size()] );
+    //check if it is a system dashboard
+    List<IBasicFile> fileList;
+    IReadAccess access = CdeEnvironment.getUserContentAccess();
+    boolean isSystem = false;
+    if ( !dashboardPath.isEmpty() ) {
+      String path = dashboardPath.toLowerCase().replaceFirst( "/", "" );
+      if ( path.startsWith( CdeEnvironment.getSystemDir() + "/" ) ) {
+        access = Utils.getAppropriateReadAccess( dashboardPath );
+        isSystem = true;
+      }
     }
 
-    return new IBasicFile[] {};
+    if ( isSystem ) {
+      fileList = access.listFiles( dir, fileFilter, 1, true, false );
+      fileList.remove( 0 ); //remove the first because the root is being added
+    } else {
+      fileList = access.listFiles( dir, fileFilter, IReadAccess.DEPTH_ZERO, true, showHiddenFiles );
+    }
+
+
+    if ( fileList != null && fileList.size() > 0 ) {
+      return fileList.toArray( new IBasicFile[ fileList.size() ] );
+    }
+
+    return new IBasicFile[] { };
   }
 
   @GET
   @Path( "/system/{path: [^?]+ }" )
   @Produces( { MediaType.WILDCARD } )
-  public Response getSystemResource( @PathParam( "path" ) String path, @Context HttpServletResponse response ) throws IOException {
-    
+  public Response getSystemResource( @PathParam( "path" ) String path, @Context HttpServletResponse response )
+    throws IOException {
+
     String pluginId = CdeEngine.getInstance().getEnvironment().getPluginId();
-    
+
     IPluginManager pluginManager = PentahoSystem.get( IPluginManager.class );
-    
-    if(!StringUtils.isEmpty( path ) && pluginManager.isPublic( pluginId, path ) ){
-      
+
+    if ( !StringUtils.isEmpty( path ) && pluginManager.isPublic( pluginId, path ) ) {
+
       Response readFileResponse = new PluginResource( response ).readFile( pluginId, path );
-      
-      if ( readFileResponse.getStatus() != Status.NOT_FOUND.getStatusCode() ) {        
+
+      if ( readFileResponse.getStatus() != Status.NOT_FOUND.getStatusCode() ) {
         return readFileResponse;
       }
     }
-    
+
     return Response.status( Status.NOT_FOUND ).build();
   }
 
