a55416a40090f35446782192e61ec9ca9b5da92f
David Santos <david.santos@webdetails.pt>
2014-05-08 14:57:19 +0100

    [CDE-139]: Standerdize preview request for system and solution

--- cde-core/src/pt/webdetails/cdf/dd/extapi/IFileHandler.java
+++ cde-core/src/pt/webdetails/cdf/dd/extapi/IFileHandler.java
@@ -2,6 +2,6 @@ package pt.webdetails.cdf.dd.extapi;
 
 public interface IFileHandler {
 
-  public boolean saveDashboardAs( String path, String title, String description, String cdfdeJsText ) throws Exception;
+  public boolean saveDashboardAs( String path, String title, String description, String cdfdeJsText, boolean isPreview ) throws Exception;
 
 }
