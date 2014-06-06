b054dbb80f803f7aeb3e8ff99e2fd9243077ff83
David Santos <david.santos@webdetails.pt>
2014-06-06 20:01:07 +0100

    [CDE-252]: Replicated javascript unit test logic to pentaho5

---
+++ cde-pentaho5/test-js/cdf-dd-base-spec.js
@@ -0,0 +1,51 @@
+describe("CDF-DD-BASE tests", function() {
+  beforeEach(function() {
+
+    cdfdd.layout = {init: function() {}};
+    cdfdd.components = {initTemplate: function() {}};
+    cdfdd.datasources = {initTemplate: function() {}};
+
+    cdfdd.dashboardData = {
+      layout: { rows: ["layout_test"]},
+      components: { rows: ["comp_test"]},
+      datasources: { rows: ["data_test"]}
+    };
+
+  });
+
+  it("test load template when the template is empty",function() {
+
+    SynchronizeRequests.selectTemplate = {
+        structure: {
+            layout: { rows: ["layout_temp"]},
+            components: { rows: []},
+            datasources: { rows: []}
+        }
+    };
+
+    SynchronizeRequests.callbackLoadTemplate(true);
+
+    expect( cdfdd.dashboardData.layout.rows[0] == "layout_temp" ).toBeTruthy()
+    expect( cdfdd.dashboardData.components.rows[0] == "comp_test" ).toBeTruthy();
+    expect( cdfdd.dashboardData.datasources.rows[0] == "data_test" ).toBeTruthy();
+
+  });
+
+  it("test load Template when the template is not empty", function() {
+
+    SynchronizeRequests.selectTemplate = {
+      "structure": {
+        layout: { "rows": ["layout_templ"]},
+        components: { rows: ["comp_templ"]},
+        datasources: { rows: ["data_templ"]}
+      }
+    };
+
+    SynchronizeRequests.callbackLoadTemplate(true);
+
+    expect( cdfdd.dashboardData.layout.rows[0] == "layout_templ" ).toBeTruthy()
+    expect( cdfdd.dashboardData.components.rows[0] == "comp_templ" ).toBeTruthy();
+    expect( cdfdd.dashboardData.datasources.rows[0] == "data_templ" ).toBeTruthy();
+  });
+
+});
