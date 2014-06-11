0507051502325cfe29135375a44b484def482c67
David Santos <david.santos@webdetails.pt>
2014-06-11 15:25:38 +0100

    [CLEANUP]: Reviewed Style and license update

--- cde-core/src/pt/webdetails/cdf/dd/render/layout/CarouselItemRender.java
+++ cde-core/src/pt/webdetails/cdf/dd/render/layout/CarouselItemRender.java
@@ -1,51 +1,54 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 
 package pt.webdetails.cdf.dd.render.layout;
 
 import org.apache.commons.jxpath.JXPathContext;
 import pt.webdetails.cdf.dd.util.XPathUtils;
 
-public class CarouselItemRender extends DivRender
-{
+public class CarouselItemRender extends DivRender {
 
-  public CarouselItemRender(JXPathContext context)
-  {
-    super(context);
+  public CarouselItemRender( JXPathContext context ) {
+    super( context );
   }
 
-  public String renderClose()
-  {
+  public String renderClose() {
     return "</div></li>";
   }
 
   @Override
-  public void processProperties()
-  {
+  public void processProperties() {
 
     super.processProperties();
-    getPropertyBag().addId(getId());
-    getPropertyBag().addClass("cdfCarouselItemContent");
+    getPropertyBag().addId( getId() );
+    getPropertyBag().addClass( "cdfCarouselItemContent" );
   }
 
   @Override
-  public String renderStart()
-  {
+  public String renderStart() {
 
     StringBuilder div = new StringBuilder();
-    div.append("<li class='cdfCarouselItem'><div class='cdfCarouselItemTitle'>");
-    div.append(getPropertyString("title"));
-    div.append("</div><div  ");
-    div.append(getPropertyBagString());
-    div.append(">");
+    div.append( "<li class='cdfCarouselItem'><div class='cdfCarouselItemTitle'>" );
+    div.append( getPropertyString( "title" ) );
+    div.append( "</div><div  " );
+    div.append( getPropertyBagString() );
+    div.append( ">" );
 
     return div.toString();
   }
 
-  protected String getId()
-  {
-    String id = getPropertyString("name");
-    return id.length() > 0 ? id : XPathUtils.getStringValue(getNode(), "id");
+  protected String getId() {
+    String id = getPropertyString( "name" );
+    return id.length() > 0 ? id : XPathUtils.getStringValue( getNode(), "id" );
   }
 }
--- cde-core/src/pt/webdetails/cdf/dd/render/layout/CarouselRender.java
+++ cde-core/src/pt/webdetails/cdf/dd/render/layout/CarouselRender.java
@@ -1,52 +1,54 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 
 package pt.webdetails.cdf.dd.render.layout;
 
 import org.apache.commons.jxpath.JXPathContext;
 import pt.webdetails.cdf.dd.util.XPathUtils;
 
-public class CarouselRender extends DivRender
-{
+public class CarouselRender extends DivRender {
 
-  public CarouselRender(JXPathContext context)
-  {
-    super(context);
+  public CarouselRender( JXPathContext context ) {
+    super( context );
   }
 
   @Override
-  public String renderClose()
-  {
+  public String renderClose() {
     return "</ul></div>";
   }
 
   @Override
-  public void processProperties()
-  {
+  public void processProperties() {
     super.processProperties();
-    getPropertyBag().addId(getId());
-    getPropertyBag().addClass("cdfCarousel");
-    if (getPropertyBoolean("showTitle"))
-    {
-      getPropertyBag().addClass("showTitle");
+    getPropertyBag().addId( getId() );
+    getPropertyBag().addClass( "cdfCarousel" );
+    if ( getPropertyBoolean( "showTitle" ) ) {
+      getPropertyBag().addClass( "showTitle" );
     }
-    getPropertyBag().addClass("cdfCarousel");
+    getPropertyBag().addClass( "cdfCarousel" );
 
   }
 
   @Override
-  public String renderStart()
-  {
+  public String renderStart() {
 
     String div = "<div class='cdfCarouselHolder'><ul ";
     div += getPropertyBagString() + ">";
     return div;
   }
 
-  protected String getId()
-  {
-    String id = getPropertyString("name");
-    return id.length() > 0 ? id : XPathUtils.getStringValue(getNode(), "id");
+  protected String getId() {
+    String id = getPropertyString( "name" );
+    return id.length() > 0 ? id : XPathUtils.getStringValue( getNode(), "id" );
   }
 }
