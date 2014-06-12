14ce0eeee6b4ab67c0900062100a66f9b36c6661
Pedro Teixeira <pedrofvteixeira@gmail.com>
2014-06-12 17:55:29 +0100

    [CDE-266] access denied while creating folder with name "cde" 	- ensureBasicDirs() now delegates the file/folder creation responsibility to each of the CDE environments

--- cde-pentaho5/src/pt/webdetails/cdf/dd/extapi/FileHandler.java
+++ cde-pentaho5/src/pt/webdetails/cdf/dd/extapi/FileHandler.java
@@ -1,20 +1,39 @@
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 package pt.webdetails.cdf.dd.extapi;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
-
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.pentaho.platform.engine.security.SecurityHelper;
 import pt.webdetails.cdf.dd.CdeConstants;
 import pt.webdetails.cdf.dd.PentahoCdeEnvironment;
 import pt.webdetails.cdf.dd.structure.DashboardStructure;
 import pt.webdetails.cdf.dd.util.CdeEnvironment;
 import pt.webdetails.cdf.dd.util.Utils;
 import pt.webdetails.cpf.impl.FileContent;
+import pt.webdetails.cpf.repository.api.IRWAccess;
 import pt.webdetails.cpf.utils.CharsetHelper;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.concurrent.Callable;
+
 public class FileHandler implements IFileHandler {
+
+  protected static Log logger = LogFactory.getLog( FileHandler.class );
 
   @Override
   public boolean saveDashboardAs( String path, String title, String description, String cdfdeJsText, boolean isPreview )
@@ -59,4 +78,60 @@ public class FileHandler implements IFileHandler {
 
   }
 
+  @Override
+  public boolean ensureFileExists( final IRWAccess access, final String file, final InputStream content ) {
+
+    if ( access == null || StringUtils.isEmpty( file ) || content == null ) {
+      return false;
+    }
+
+    // skip creation if file already exists
+    if ( !access.fileExists( file ) ) {
+
+      try {
+        // current user may not have necessary create permissions; this is an admin task
+        SecurityHelper.getInstance().runAsSystem( new Callable<Boolean>() {
+
+          @Override
+          public Boolean call() throws Exception {
+            return access.saveFile( file, content );
+          }
+        } );
+
+      } catch ( Exception e ) {
+        logger.error( "Couldn't find or create CDE " + file + "  file", e );
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  @Override
+  public boolean ensureDirExists( final IRWAccess access, final String relativeFolderPath ) {
+
+    if ( access == null || StringUtils.isEmpty( relativeFolderPath ) ) {
+      return false;
+    }
+
+    // skip creation if folder already exists
+    if ( !access.fileExists( relativeFolderPath ) ) {
+
+      try {
+        // current user may not have necessary create permissions; this is an admin task
+        SecurityHelper.getInstance().runAsSystem( new Callable<Boolean>() {
+
+          @Override
+          public Boolean call() throws Exception {
+            return access.createFolder( relativeFolderPath );
+          }
+        } );
+      } catch ( Exception e ) {
+        logger.error( "Couldn't find or create CDE " + relativeFolderPath + "  dir", e );
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
