78dcef5cb94f895e0d210166252e791ace496ec5
Joao Velasques <joao.velasques@webdetails.pt>
2014-06-16 10:27:11 +0100

    [TEST] - Test fixing

--- cde-pentaho5/test-src/pt/webdetails/cdf/dd/model/inst/writer/cdfrunjs/dashboard/PentahoCdfRunJsDashboardWriteContextForTesting.java
+++ cde-pentaho5/test-src/pt/webdetails/cdf/dd/model/inst/writer/cdfrunjs/dashboard/PentahoCdfRunJsDashboardWriteContextForTesting.java
@@ -1,3 +1,16 @@
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
+
 package pt.webdetails.cdf.dd.model.inst.writer.cdfrunjs.dashboard;
 
 import pt.webdetails.cdf.dd.model.core.writer.IThingWriterFactory;
@@ -11,7 +24,13 @@ public class PentahoCdfRunJsDashboardWriteContextForTesting extends PentahoCdfRu
     super( factory, indent, bypassCacheRead, dash, options );
   }
 
-  @Override protected String getRoot() {
+  @Override
+  protected String getRoot() {
     return "";
+  }
+
+  @Override
+  protected String getSystemDir() {
+    return "system";
   }
 }
