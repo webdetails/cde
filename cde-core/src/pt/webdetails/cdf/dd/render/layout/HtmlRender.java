383dc24dd6de84eb42269e64ef5ee411f90f49cf
David Santos <david.santos@webdetails.pt>
2014-06-19 13:51:37 +0100

    [CLEANUP]: License update and code style review

--- cde-core/src/pt/webdetails/cdf/dd/render/layout/HtmlRender.java
+++ cde-core/src/pt/webdetails/cdf/dd/render/layout/HtmlRender.java
@@ -1,6 +1,15 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 
 package pt.webdetails.cdf.dd.render.layout;
 
@@ -8,48 +17,48 @@ import org.apache.commons.jxpath.JXPathContext;
 
 public class HtmlRender extends Render {
 
-    private boolean hasProperties = false;
+  private boolean hasProperties = false;
 
-    public HtmlRender(JXPathContext context) {
-        super(context);
+  public HtmlRender( JXPathContext context ) {
+    super( context );
+  }
+
+  @Override
+  public void processProperties() {
+
+    getPropertyBag().addStyle( "color", getPropertyString( "color" ) );
+    getPropertyBag().addClass( getPropertyString( "cssClass" ) );
+    String fontSize = getPropertyString( "fontSize" );
+    if ( fontSize.length() > 0 ) {
+      getPropertyBag().addStyle( "font-size", fontSize + "px" );
     }
 
-    @Override
-    public void processProperties() {
-
-        getPropertyBag().addStyle("color", getPropertyString("color"));
-        getPropertyBag().addClass(getPropertyString("cssClass"));
-        String fontSize = getPropertyString("fontSize");
-        if (fontSize.length() > 0) {
-            getPropertyBag().addStyle("font-size", fontSize + "px");
-        }
-
-        if (getPropertyBagString().length() > 0) {
-            hasProperties = true;
-        }
-        
-        getPropertyBag().addClass(getPropertyString("cssClass"));
-
+    if ( getPropertyBagString().length() > 0 ) {
+      hasProperties = true;
     }
 
-    public String renderStart() {
+    getPropertyBag().addClass( getPropertyString( "cssClass" ) );
 
-        String out = "";
-        if (hasProperties) {
-            out += "<span " + getPropertyBagString() + ">";
-        }
+  }
 
-        out += getPropertyString("html");
-        return out;
+  public String renderStart() {
 
+    String out = "";
+    if ( hasProperties ) {
+      out += "<span " + getPropertyBagString() + ">";
     }
 
-    public String renderClose() {
+    out += getPropertyString( "html" );
+    return out;
 
-        if (hasProperties) {
-            return "</span>";
-        } else {
-            return "";
-        }
+  }
+
+  public String renderClose() {
+
+    if ( hasProperties ) {
+      return "</span>";
+    } else {
+      return "";
     }
+  }
 }
