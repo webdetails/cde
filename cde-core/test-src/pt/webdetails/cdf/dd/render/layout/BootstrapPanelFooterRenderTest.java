5bfeca65d30328105c492a3a83e74b75b14389a2
David Santos <david.santos@webdetails.pt>
2014-06-20 16:58:28 +0100

    [CDE-253]: Unit Tests for Bootstrap Panel layout component

--- cde-core/test-src/pt/webdetails/cdf/dd/render/layout/BootstrapPanelFooterRenderTest.java
+++ cde-core/test-src/pt/webdetails/cdf/dd/render/layout/BootstrapPanelFooterRenderTest.java
@@ -13,24 +13,36 @@
 
 package pt.webdetails.cdf.dd.render.layout;
 
+import junit.framework.Assert;
+import junit.framework.TestCase;
 import org.apache.commons.jxpath.JXPathContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 
-public class BootstrapPanelRender extends DivRender {
+public class BootstrapPanelFooterRenderTest extends TestCase {
 
-  public BootstrapPanelRender( JXPathContext context ) {
-    super( context );
+  private BootstrapPanelFooterRender renderForTest;
+
+  @Before
+  public void setUp() throws Exception {
+    JXPathContext context = Mockito.mock( JXPathContext.class );
+    renderForTest = new BootstrapPanelFooterRender( context );
+    renderForTest.processProperties();
   }
 
-  @Override
-  public void processProperties() {
-    super.processProperties();
+  @Test
+  public void testRenderStart() {
+    String div = renderForTest.renderStart();
 
-    getPropertyBag().addClass( "panel" );
-    getPropertyBag().addClass( getBootstrapPanelStyle() );
+    Assert.assertEquals( "<div  class='panel-footer ' >", div );
   }
 
-  protected String getBootstrapPanelStyle() {
-    return getPropertyString( "bootstrapPanelStyle" );
+  @Test
+  public void testRenderClose() {
+    String div = renderForTest.renderClose();
+
+    Assert.assertEquals( "</div>", div );
   }
 
 }
