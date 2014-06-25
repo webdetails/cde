36c574e14757260369029797242fce14892917ae
Pedro Teixeira <pedrofvteixeira@gmail.com>
2014-06-25 09:51:16 -0400

    [CDE-179] After CDF core-refactor becomes master, remove cggHelper.js from CDE-pentaho/CDE-pentaho5

--- cde-pentaho/resource/js/cggHelper.js
+++
@@ -1,8 +0,0 @@
-wd = wd || {};
-wd.helpers = wd.helpers || {};
-
-wd.helpers.cggHelper = {
-    getCggDrawUrl: function(){
-        return Dashboards.getWebAppPath() + "/content/cgg/draw";
-    }
-}
