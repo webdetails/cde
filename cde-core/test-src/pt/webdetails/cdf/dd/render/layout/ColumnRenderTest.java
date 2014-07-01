099d5fddcbadfc7f5002ffc70fedc9080c186812
David Santos <david.santos@webdetails.pt>
2014-07-01 13:22:59 +0100

    [CDE-291]: Unit Test for Layout Column Render

---
+++ cde-core/test-src/pt/webdetails/cdf/dd/render/layout/ColumnRenderTest.java
@@ -0,0 +1,76 @@
+/*!
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
+
+package pt.webdetails.cdf.dd.render.layout;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.apache.commons.jxpath.JXPathContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ColumnRenderTest extends TestCase {
+
+  private ColumnRenderForTest renderForTest;
+
+  private class ColumnRenderForTest extends ColumnRender {
+
+    String testRenderType;
+
+    public ColumnRenderForTest( JXPathContext context ) {
+      super( context );
+    }
+
+    @Override
+    protected boolean lastColumn() {
+      return false;
+    }
+
+    @Override
+    protected String getRenderType() {
+      return testRenderType;
+    }
+
+    private void setTestRenderType( String type ) {
+      this.testRenderType = type;
+    }
+
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    JXPathContext context = Mockito.mock( JXPathContext.class );
+    renderForTest = new ColumnRenderForTest( context );
+  }
+
+   @Test
+   public void testBootstrapRender() {
+     renderForTest.setTestRenderType( "bootstrap" );
+     renderForTest.addColSpan( "5" );
+     String div = renderForTest.renderStart();
+
+     Assert.assertEquals( "<div  class='col-md-5 ' >", div );
+
+   }
+
+  @Test
+  public void testBlueprintRender() {
+    renderForTest.setTestRenderType( "blueprint" );
+    renderForTest.addColSpan( "5" );
+    String div = renderForTest.renderStart();
+
+    Assert.assertEquals( "<div  class='span-5 ' >", div );
+
+  }
+}
