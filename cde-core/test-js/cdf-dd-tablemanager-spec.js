ff80bb5e702759394b7238b715c90140917bd610
David Santos <david.santos@webdetails.pt>
2014-09-09 18:38:59 +0100

    [CDE-353]: fixed relative system resources path in client side and updated unit test

--- cde-core/test-js/cdf-dd-tablemanager-spec.js
+++ cde-core/test-js/cdf-dd-tablemanager-spec.js
@@ -32,10 +32,65 @@ describe("CDF-DD-TABLEMANAGER-TESTS", function() {
 
     // - editing a dashboard
     // system resources always have the plugin folder as root
-    CDFDDFileName = "/system/myPlugin/myDashboard.wcdf";
-    expect(resourceFileRenderer.formatSelection(file1)).toBe("${system:/myResource.css}");
-    expect(resourceFileRenderer.formatSelection(file2)).toBe("${system:/resources/myResource.css}");
-    expect(resourceFileRenderer.formatSelection(file3)).toBe("${system:/mySolution/resources/myResource.css}");
+    CDFDDFileName = "/system/myPlugin/mySolution/myDashboard.wcdf";
+    expect(resourceFileRenderer.formatSelection(file1)).toBe("${system:../myResource.css}");
+    expect(resourceFileRenderer.formatSelection(file2)).toBe("${system:../resources/myResource.css}");
+    expect(resourceFileRenderer.formatSelection(file3)).toBe("${system:resources/myResource.css}");
+  });
+
+  it("correctly gets fileName before opening External Editor", function() {
+    var tableManager = new TableManager("test-tableManager");
+    var resourceFileRenderer = new ResourceFileRenderer(tableManager);
+
+    var file1 = "/myResource.css",
+        file2 = "/resources/myResource.css",
+        file3 = "/mySolution/resources/myResource.css";
+
+    //solution dashboards
+    // - new dashboard
+    CDFDDFileName = "";
+    expect(resourceFileRenderer.getFileName("${solution:/myResource.css}"))
+        .toBe(file1);
+    expect(resourceFileRenderer.getFileName("${solution:/resources/myResource.css}"))
+        .toBe(file2);
+    expect(resourceFileRenderer.getFileName("${solution:/mySolution/resources/myResource.css}"))
+        .toBe(file3);
+
+    // - editing a dashboard
+    CDFDDFileName = "/mySolution/myDashboard.wcdf";
+    expect(resourceFileRenderer.getFileName("${solution:/myResource.css}"))
+        .toBe(file1);
+    expect(resourceFileRenderer.getFileName("${solution:../myResource.css}"))
+        .toBe(file1);
+    expect(resourceFileRenderer.getFileName("${solution:../resources/myResource.css}"))
+        .toBe(file2);
+    expect(resourceFileRenderer.getFileName("${solution:resources/myResource.css}"))
+        .toBe(file3);
+
+    //system dashboards
+    // - new dashboard
+    // this case is not applicable because cde new dashboard is only used in solution
+    // system dashboards are created and always edited from a previously known file
+    CDFDDFileName = "";
+    expect(resourceFileRenderer.getFileName("${solution:/myResource.css}"))
+        .toBe(file1);
+    expect(resourceFileRenderer.getFileName("${solution:/resources/myResource.css}"))
+        .toBe(file2);
+    expect(resourceFileRenderer.getFileName("${solution:/mySolution/resources/myResource.css}"))
+        .toBe(file3);
+
+    // - editing a dashboard
+    // system resources always have the plugin folder as root
+    CDFDDFileName = "/system/myPlugin/mySolution/myDashboard.wcdf";
+    var systemCtx = "/system/myPlugin";
+    expect(resourceFileRenderer.getFileName("${system:/myResource.css}"))
+        .toBe(file1);
+    expect(resourceFileRenderer.getFileName("${system:../myResource.css}"))
+        .toBe(systemCtx + file1);
+    expect(resourceFileRenderer.getFileName("${system:../resources/myResource.css}"))
+        .toBe(systemCtx + file2)
+    expect(resourceFileRenderer.getFileName("${system:resources/myResource.css}"))
+        .toBe(systemCtx + file3);
   });
 
 });
