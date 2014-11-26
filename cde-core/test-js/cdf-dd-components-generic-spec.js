99cf7ef149c8565e4a87d833935cdbc27f4f3021
David Santos <david.santos@webdetails.pt>
2014-11-26 19:05:51 +0000

    [CDE-413]: 'Column Types' editor has a poor layout (tableComponent)

--- cde-core/test-js/cdf-dd-components-generic-spec.js
+++ cde-core/test-js/cdf-dd-components-generic-spec.js
@@ -1,7 +1,71 @@
 describe("CDF-DD-COMPONENTS-GENERIC-TESTS", function() {
+  var tableManager = new TableManager('test-tableManager');
+
+  describe("Testing ValuesArrayRenderer #", function() {
+    var valueAr = new ValuesArrayRenderer(tableManager);
+
+    it("# Object Initial Values", function() {
+      expect(valueAr.multiDimensionArray).toBe(true);
+      expect(valueAr.cssPrefix).toEqual('StringList');
+      expect(valueAr.hasTypedValues).toBe(false);
+      expect(valueAr.argTitle).toEqual('Arg');
+      expect(valueAr.valTitle).toEqual('Value');
+      expect(valueAr.typesArray).toEqual([]);
+      expect(valueAr.selectData).toEqual({});
+    });
+
+    it("# addParameter", function() {
+      spyOn(valueAr, 'addTypedParameters');
+      spyOn(valueAr, 'addParameters');
+
+      var values1 = ["arg","",""];
+      var values2 = ["arg", "value"];
+      var values3 = ["arg", null];
+      var values4 = ["arg", undefined];
+
+      var index = 0;
+      var container = undefined;
+      valueAr.addParameter(index, values1, container);
+      expect(valueAr.addParameters).toHaveBeenCalledWith(index, values1[0], values1[1], container);
+      valueAr.addParameter(index, values2, container);
+      expect(valueAr.addParameters).toHaveBeenCalledWith(index, values2[0], values2[1], container);
+      valueAr.addParameter(index, values3, container);
+      expect(valueAr.addParameters).toHaveBeenCalledWith(index, values3[0], 'null', container);
+      valueAr.addParameter(index, values4, container);
+      expect(valueAr.addParameters).toHaveBeenCalledWith(index, values4[0], '', container);
+
+      expect(valueAr.addTypedParameters).not.toHaveBeenCalled();
+    });
+
+    it("# getParameterValues", function() {
+      //it is a multi dimensional array without typed values
+      expect( valueAr.getParameterValues(0).length ).toEqual(2)
+    });
+  });
+
+  describe("Testing ColTypesArrayRender #", function() {
+    var ctar = new ColTypesArrayRender(tableManager);
+
+    it("# Object Initial Values", function() {
+      expect(ctar.multiDimensionArray).toBe(false);
+      expect(ctar.cssPrefix).toEqual('StringArray');
+      expect(ctar.hasTypedValues).toBe(false);
+      expect(ctar.argTitle).toEqual('Arg');
+      expect(ctar.valTitle).toEqual('Value');
+      expect(ctar.typesArray).toEqual([]);
+      expect(ctar.selectData).toEqual({});
+    });
+
+    it("# getData", function() {
+      ctar.selectData = {};
+      expect(ctar.getData()).toEqual({string: 'string', numeric: 'numeric'});
+
+      ctar.selectData = { preData: 'value' };
+      expect(ctar.getData()).toEqual({ preData: 'value', string: 'string', numeric: 'numeric' });
+    });
+  });
 
   describe("Testing EditorValuesArrayRenderer #", function() {
-    var tableManager = new TableManager("test-tableManager");
     var evar = new EditorValuesArrayRenderer(tableManager);
     evar.cssClass = "css";
     evar.cssPrefix = "StringList";
