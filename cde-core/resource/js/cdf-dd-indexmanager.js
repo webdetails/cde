45d49b744fd9ddbf2a73d58e7991eacc9d934c16
David Santos <david.santos@webdetails.pt>
2014-12-15 19:32:12 +0000

    [CLEANUP]: license update and some code style review

--- cde-core/resource/js/cdf-dd-indexmanager.js
+++ cde-core/resource/js/cdf-dd-indexmanager.js
@@ -1,131 +1,140 @@
+/*!
+ * Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
 
 // IndexManager
-
 var IndexManager = Base.extend({
 
-		id: "",
-		logger: {},
-		index: {},
-		
-		
+  id: "",
+  logger: {},
+  index: {},
 
-		constructor: function(tableModel){
-			this.logger = new Logger("IndexManager");
-			this.setTableModel(tableModel);
-		},
+  constructor: function(tableModel) {
+    this.logger = new Logger("IndexManager");
+    this.setTableModel(tableModel);
+  },
 
-		
-		updateIndex: function(){
+  updateIndex: function() {
+    var data = this.getTableModel().getData();
+    var idx = {};
+    var index = 0;
 
-			var data = this.getTableModel().getData();
-			var idx = {};
-			var index = 0;
+    // Add root entry
+    var root = {};
+    root.id = IndexManager.ROOTID;
+    root.parent = null;
+    root.children = [];
+    root.type = _type;
+    root.index = index;
+    idx[IndexManager.ROOTID] = root;
 
-			// Add root entry
-			var root = {};
-			root.id = IndexManager.ROOTID;
-			root.parent = null;
-			root.children = [];
-			root.type = _type;
-			root.index = index;
-			idx[IndexManager.ROOTID] = root; 
+    for(var rowIndex in data) {
+      if(data.hasOwnProperty(rowIndex)) {
+        rowIndex = parseFloat(rowIndex);
+        if(rowIndex != NaN) {
+          var row = data[rowIndex];
+          var _id = this.getTableModel().getEvaluatedId(rowIndex);
+          var _parent;
+          if(typeof this.getTableModel().getParentId() == 'undefined') {
+            // Parent folder
+            _parent = IndexManager.ROOTID;
+          } else {
+            // Child folder
+            _parent = this.getTableModel().getParentId()(row);
+            if(typeof _parent == 'undefined') {
+              _parent = IndexManager.ROOTID;
+            }
+          }
+          var _type = this.getTableModel().getRowType()(row);
 
-			for ( var rowIndex in data){
-				if(data.hasOwnProperty(rowIndex)){
-					rowIndex = parseFloat(rowIndex);
-					if(rowIndex != NaN){
-						var row = data[rowIndex];
-						var _id = this.getTableModel().getEvaluatedId(rowIndex);
-						var _parent;
-						if (typeof this.getTableModel().getParentId() == 'undefined'){
-							// Parent folder
-							_parent = IndexManager.ROOTID;
-						}
-						else{
-							// Child folder
-							_parent = this.getTableModel().getParentId()(row);
-							if (typeof _parent == 'undefined'){
-								_parent = IndexManager.ROOTID;
-							}
-						}
-						var _type = this.getTableModel().getRowType()(row) ;
+          var entry = {};
+          entry.id = _id;
+          entry.parent = _parent;
+          entry.children = [];
+          entry.type = _type;
+          entry.index = rowIndex;
+          idx[_id] = entry;
+          idx[_parent].children.push(entry);
+        }
 
-						var entry = {};
-						entry.id = _id;
-						entry.parent = _parent;
-						entry.children = [];
-						entry.type = _type;
-						entry.index = rowIndex;
-						idx[_id] = entry; 
-						idx[_parent].children.push(entry);
-					}
-					index++;
-				}
-			};
+        index++;
+      }
+    }
 
-			this.setIndex(idx);
+    this.setIndex(idx);
+  },
 
-		},
+  isFirstChild: function(rowId) {
+    return this.getIndex()[this.getIndex()[rowId].parent].children[0].id === rowId;
+  },
 
-		isFirstChild: function(rowId){
-		
-			return this.getIndex()[this.getIndex()[rowId].parent].children[0].id === rowId;
-		},
+  isLastChild: function(rowId) {
+    var _brothers = this.getBrothers(rowId);
 
-		isLastChild: function(rowId){
-			var _brothers = this.getBrothers(rowId);
-			return _brothers[_brothers.length-1].id === rowId;
+    return _brothers[_brothers.length - 1].id === rowId;
+  },
 
-		},
+  getBrothers: function(rowId) {
+    return this.getIndex()[this.getIndex()[rowId].parent].children;
 
-		getBrothers: function(rowId){
-			return this.getIndex()[this.getIndex()[rowId].parent].children;
-		
-		},
+  },
 
-		getChildIndex: function(rowId){
-			
-			var brothers = this.getBrothers(rowId);
-			var idx = -1;
-			$.each(brothers,function(i,brother){
-					if (brothers[i].id === rowId){
-						idx = i;
-						return false;
-					}
-				});
-			return idx;
-		
-		},
+  getChildIndex: function(rowId) {
 
-		getLastChild: function(rowId){
-			
-			var _children = this.getIndex()[rowId].children;
-			var _length = _children.length;
-			if (_length == 0){
-				return this.getIndex()[rowId];
-			}
-			else{
-				return this.getLastChild(_children[_length-1].id);
-			}
-		
-		},
+    var brothers = this.getBrothers(rowId);
+    var idx = -1;
+    $.each(brothers, function(i, brother) {
+      if(brothers[i].id === rowId) {
+        idx = i;
+        return false;
+      }
+    });
 
+    return idx;
+  },
 
-		getPreviousSibling: function(rowId){
-			return this.getBrothers(rowId)[this.getChildIndex(rowId) - 1];
-		},
+  getLastChild: function(rowId) {
+    var _children = this.getIndex()[rowId].children;
+    var _length = _children.length;
+    if(_length == 0) {
+      return this.getIndex()[rowId];
+    } else {
+      return this.getLastChild(_children[_length - 1].id);
+    }
+  },
 
-		getNextSibling: function(rowId){
-			return this.getBrothers(rowId)[this.getChildIndex(rowId) + 1];
-		},
+  getPreviousSibling: function(rowId) {
+    return this.getBrothers(rowId)[this.getChildIndex(rowId) - 1];
+  },
 
-		setIndex: function(index){this.index = index;}, //XXX - index is an object!
-		getIndex: function(){return this.index;},
-		setTableModel: function(tableModel){this.tableModel = tableModel; this.updateIndex();},
-		getTableModel: function(){return this.tableModel;}
+  getNextSibling: function(rowId) {
+    return this.getBrothers(rowId)[this.getChildIndex(rowId) + 1];
+  },
 
-	},{
-		ROOTID: "UnIqEiD"
-	});
+  setIndex: function(index) {
+    this.index = index;
+  }, //XXX - index is an object!
+  getIndex: function() {
+    return this.index;
+  },
+  setTableModel: function(tableModel) {
+    this.tableModel = tableModel;
+    this.updateIndex();
+  },
+  getTableModel: function() {
+    return this.tableModel;
+  }
+}, {
+  ROOTID: "UnIqEiD"
+});
 
 
