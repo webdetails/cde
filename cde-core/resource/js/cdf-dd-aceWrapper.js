c9a93f9a5dbd4ab4c58048bb0caab6d0bfe21f04
David Santos <david.santos@webdetails.pt>
2014-12-15 14:46:37 +0000

    cherry-pick '[CDE-403]: Edit/Add fields doesn't change focus'

--- cde-core/resource/js/cdf-dd-aceWrapper.js
+++ cde-core/resource/js/cdf-dd-aceWrapper.js
@@ -1,139 +1,134 @@
+/*!
+ * Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+*/
+
 //ACE wrapper
 var CodeEditor = Base.extend({
-	
-	MODES : {
+
+  MODES: {
     JAVASCRIPT: 'javascript',
     CSS: 'css',
     XML: 'xml'
   },
-  MODE_BASE : 'ace/mode/',
-	DEFAULT_MODE: 'text',
-	
-	modeMap :
-	{ //highlight modes
-		'css' : 'css',
-		'javascript' : 'javascript',
-		'js' : 'javascript',
-		'xml' : 'xml',
-		'cda' : 'xml',
-		'cdv' : 'javascript',
-		'html': 'html',
-		'sql' : 'text',
-		'mdx' : 'text'
-	},
-  
+  MODE_BASE: 'ace/mode/',
+  DEFAULT_MODE: 'text',
+
+  modeMap: { //highlight modes
+    'css': 'css',
+    'javascript': 'javascript',
+    'js': 'javascript',
+    'xml': 'xml',
+    'cda': 'xml',
+    'cdv': 'javascript',
+    'html': 'html',
+    'sql': 'text',
+    'mdx': 'text'
+  },
+
   mode: 'javascript',
   theme: 'ace/theme/textmate',
   editor: null,
   editorId: null,
-	
-	initEditor: function(editorId){
-			this.editor = ace.edit(editorId); 
-			this.editorId = editorId;
-			this.setMode(null);
-			this.setTheme(null);
 
+  initEditor: function(editorId) {
+    this.editor = ace.edit(editorId);
+    this.editorId = editorId;
+    this.setMode(null);
+    this.setTheme(null);
+  },
 
+  loadFile: function(fileName) {
+    var myself = this;
+    //check edit permission
+    $.get(ExternalEditor.getCanEditUrl(), { path: fileName }, function(result) {
+      var readonly = result != 'true';
+      myself.setReadOnly(readonly);
+      //TODO: can read?..get permissions?...
 
-		/*
-		this.editor = ace.edit(editorId);
-    this.editor.setTheme(this.theme);
-    
-		if(this.mode != null){
-			this.setMode(this.mode);
-		}
-    this.editor.setShowPrintMargin(false);*/
-	},
-	
-	loadFile: function(fileName){
-		var myself = this;
-		//check edit permission
-		$.get(ExternalEditor.getCanEditUrl(), {path: fileName},
-			function(result){
-				var readonly = result != 'true';
-				myself.setReadOnly(readonly);
-				//TODO: can read?..get permissions?...
+      //load file contents
+      $.get(ExternalEditor.getGetUrl(), { path: fileName }, function(fileContents) {
+        myself.setContents(fileContents);
+      });
+    });
+  },
 
-				//load file contents
-				$.get(ExternalEditor.getGetUrl(),{path:fileName},
-					function(fileContents) {
-						myself.setContents(fileContents);
-					}
-				);
-				
-			}
-		);
-	},
-	
-	setContents: function(contents){
-		this.editor.getSession().setValue(contents);
-		$(this.editorId).css("font-size","12px");
-		//this.editor.gotoLine(2);
-		//document.getElementById('codeArea').style.fontSize='12px';
+  setContents: function(contents) {
+    this.editor.getSession().setValue(contents);
+    $(this.editorId).css("font-size", "12px");
+    this.editor.focus();
+  },
 
-		//this.editor.navigateFileStart();
-	},
-	
-	saveFile: function(fileName, contents, callback){
-        $.ajax({
-            url: ExternalEditor.getWriteUrl(),
-            type: "POST",
-            dataType: "xml",
-            data: { path: fileName, data: contents },
-            complete: function(data){
-                if(typeof callback == 'function'){
-                    callback(data);
-                }
-            }
-        });
-	},
-	
-	getContents: function(){
-		return this.editor.getSession().getValue();
-	},
-	
-	setMode: function(mode)
-	{
-		this.mode = this.modeMap[mode];
+  saveFile: function(fileName, contents, callback) {
+    $.ajax({
+      url: ExternalEditor.getWriteUrl(),
+      type: "POST",
+      dataType: "xml",
+      data: {
+        path: fileName,
+        data: contents
+      },
+      complete: function(data) {
+        if(typeof callback == 'function') {
+          callback(data);
+        }
+      }
+    });
+  },
 
-	    if(this.mode == null){
-	      this.mode = this.DEFAULT_MODE;
-	    }
-    
-		if(this.editor != null)
-		{
-			if(this.mode != null){
-				var HLMode = ace.require(this.MODE_BASE + this.mode).Mode;
-				this.editor.getSession().setMode(new HLMode());
-			}
-		}
-		
-	},
-	
-	setTheme: function(themePath){
-		this.editor.setTheme((themePath == null || themePath == undefined) ? this.theme : themePath);
-		
-	},
-	
-	setReadOnly: function(readOnly){
-		if(readOnly == this.editor.getReadOnly()){ return; }
-		else{ this.editor.setReadOnly(readOnly); }
-	},
-	
-	isReadOnly: function(){
-		return this.editor.getReadOnly();
-	},
-	
-	insert: function(text){
-		this.editor.insert(text);
-	},
-	
-	getEditor: function(){
-		return this.editor;
-	},
-	
-	onChange: function(callback){
-		this.editor.getSession().on('change', callback);
-	}
-	
+  getContents: function() {
+    return this.editor.getSession().getValue();
+  },
+
+  setMode: function(mode) {
+    this.mode = this.modeMap[mode];
+
+    if(this.mode == null) {
+      this.mode = this.DEFAULT_MODE;
+    }
+
+    if(this.editor != null) {
+      if(this.mode != null) {
+        var HLMode = ace.require(this.MODE_BASE + this.mode).Mode;
+        this.editor.getSession().setMode(new HLMode());
+      }
+    }
+
+  },
+
+  setTheme: function(themePath) {
+    this.editor.setTheme((themePath == null || themePath == undefined) ? this.theme : themePath);
+  },
+
+  setReadOnly: function(readOnly) {
+    if(readOnly == this.editor.getReadOnly()) {
+      return;
+    } else {
+      this.editor.setReadOnly(readOnly);
+    }
+  },
+
+  isReadOnly: function() {
+    return this.editor.getReadOnly();
+  },
+
+  insert: function(text) {
+    this.editor.insert(text);
+  },
+
+  getEditor: function() {
+    return this.editor;
+  },
+
+  onChange: function(callback) {
+    this.editor.getSession().on('change', callback);
+  }
 });
