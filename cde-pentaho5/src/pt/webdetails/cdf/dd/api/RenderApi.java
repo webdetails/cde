abf06ddc67e91b79d08dabd3d844f8ec109e4151
Joao Velasques <joao.velasques@webdetails.pt>
2015-02-13 19:52:28 +0000

    cherry-pick '[CDE-445] - CDE audit logging causing Pentaho Operations Mart ETL to fail'

--- cde-pentaho5/src/pt/webdetails/cdf/dd/api/RenderApi.java
+++ cde-pentaho5/src/pt/webdetails/cdf/dd/api/RenderApi.java
@@ -138,14 +138,17 @@ public class RenderApi {
                         @QueryParam( MethodParams.VIEWID ) @DefaultValue( "" ) String viewId,
                         @QueryParam( MethodParams.STYLE ) @DefaultValue( "" ) String style,
                         @Context HttpServletRequest request ) throws IOException {
-
     String schemeToUse = "";
     if (!inferScheme) {
       schemeToUse = StringUtils.isEmpty( scheme ) ? request.getScheme() : scheme;
     }
-    String filePath = getWcdfRelativePath( solution, path, file ); //FIXME Util.joinPath
-    IReadAccess readAccess = Utils.getSystemOrUserReadAccess( filePath );
 
+    String filePath = getWcdfRelativePath( solution, path, file ); //FIXME Util.joinPath
+    if ( StringUtils.isEmpty( filePath ) ) {
+      return "No path provided.";
+    }
+
+    IReadAccess readAccess = Utils.getSystemOrUserReadAccess( filePath );
     if ( readAccess == null ) {
       return "Access Denied or File Not Found.";
     }
