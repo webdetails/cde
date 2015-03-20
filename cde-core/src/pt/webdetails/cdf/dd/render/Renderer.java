1c8c364c7e258a6dfe1037fb6c55172b3767d4f6
David Santos <david.santos@webdetails.pt>
2015-03-20 15:09:27 +0000

    [CLEANUP] license update and code style review

--- cde-core/src/pt/webdetails/cdf/dd/render/Renderer.java
+++ cde-core/src/pt/webdetails/cdf/dd/render/Renderer.java
@@ -1,6 +1,15 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*!
+* Copyright 2002 - 2015 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 
 package pt.webdetails.cdf.dd.render;
 
@@ -48,7 +57,7 @@ public abstract class Renderer {
    */
   protected final Map<String, CdfRunJsDashboardWriteResult> getWidgets( String aliasPrefix ) {
     Map<String, CdfRunJsDashboardWriteResult> widgetsByContainerId =
-        new HashMap<String, CdfRunJsDashboardWriteResult>();
+      new HashMap<String, CdfRunJsDashboardWriteResult>();
 
     Dashboard dashboard = this.getContext().getDashboard();
     if ( dashboard.getRegularCount() > 0 ) {
@@ -68,7 +77,8 @@ public abstract class Renderer {
             logger.error( "Could not render widget '" + widgetComp.getWcdfPath() + "'", ex );
           }
 
-          String containerId = widgetComp.tryGetPropertyValue( "htmlObject", "" ).replaceAll( "\\$\\{.*:(.*)\\}", "$1" );
+          String containerId =
+            widgetComp.tryGetPropertyValue( "htmlObject", "" ).replaceAll( "\\$\\{.*:(.*)\\}", "$1" );
           widgetsByContainerId.put( containerId, dashResult );
         }
       }
@@ -102,7 +112,7 @@ public abstract class Renderer {
   }
 
   protected final String getIndent( int indent ) {
-    switch ( indent ) {
+    switch( indent ) {
       case 0:
         return "";
       case 1:
--- cde-core/src/pt/webdetails/cdf/dd/render/cda/Variables.java
+++ cde-core/src/pt/webdetails/cdf/dd/render/cda/Variables.java
@@ -1,6 +1,15 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*!
+* Copyright 2002 - 2015 Webdetails, a Pentaho company.  All rights reserved.
+*
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 
 package pt.webdetails.cdf.dd.render.cda;
 
@@ -10,53 +19,42 @@ import org.apache.commons.jxpath.JXPathContext;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-/**
- *
- * @author pdpi
- */
-public class Variables implements CdaElementRenderer
-{
+public class Variables implements CdaElementRenderer {
 
   private JSONObject definition;
   private JXPathContext context;
 
   public Variables() {
   }
+
   public Variables( JXPathContext context ) {
     this.context = context;
   }
 
-  public void renderInto(Element dataAccess)
-  {
-    JSONArray vars = JSONArray.fromObject(definition.getString("value"));
-    if (vars.size() == 0)
-    {
+  public void renderInto( Element dataAccess ) {
+    JSONArray vars = JSONArray.fromObject( definition.getString( "value" ) );
+    if ( vars.size() == 0 ) {
       return;
     }
     Document doc = dataAccess.getOwnerDocument();
-    for (Object o : vars.toArray())
-    {
+    for ( Object o : vars.toArray() ) {
       JSONArray jsa = (JSONArray) o;
-      Element variable = doc.createElement("variables");
-      if (!jsa.getString(0).equals(""))
-      {
-        variable.setAttribute("datarow-name", jsa.getString(0));
+      Element variable = doc.createElement( "variables" );
+      if ( !jsa.getString( 0 ).equals( "" ) ) {
+        variable.setAttribute( "datarow-name", jsa.getString( 0 ) );
       }
-      if (!jsa.getString(1).equals(""))
-      {
-        variable.setAttribute("variable-name", jsa.getString(1));
+      if ( !jsa.getString( 1 ).equals( "" ) ) {
+        variable.setAttribute( "variable-name", jsa.getString( 1 ) );
       }
-      dataAccess.appendChild(variable);
+      dataAccess.appendChild( variable );
     }
   }
 
-  public void setDefinition(JSONObject definition)
-  {
+  public void setDefinition( JSONObject definition ) {
     this.definition = definition;
   }
 
-  public void setContext(JXPathContext context)
-  {
+  public void setContext( JXPathContext context ) {
     this.context = context;
   }
 }
