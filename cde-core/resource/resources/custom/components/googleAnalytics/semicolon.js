fdd5af7303ea3a6d9812b95602b72796ec2eeda3
João Velasques <joao.velasques@webdetails.pt>
2015-04-23 12:23:03 +0100

    [CDE-524] - Adopting a defensive strategy for Immediately-Invoked Function Expressions

---
+++ cde-core/resource/resources/custom/components/googleAnalytics/semicolon.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright 2002 - 2015 Webdetails, a Pentaho company. All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
+
+/*This is to add as a dependency in custom components which use libraries with 
+  Immediately-Invoked Function Expressions (IIFE), to prevent cases in which the minification
+  would put an unfinished statement before an IIFE - http://jira.pentaho.com/browse/CDE-524. 
+*/
+;
