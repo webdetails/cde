20af15067ac54089632adabf5eefa53b1d70eb2d
Carlos Russo <carlos.russo@webdetails.pt>
2015-09-29 08:25:11 +0100

    [CLEANUP] The "Shape Definition" property should be optional

--- cde-core/resource/resources/custom/components/NewMapComponent/NewMapComponent.js
+++ cde-core/resource/resources/custom/components/NewMapComponent/NewMapComponent.js
@@ -349,7 +349,7 @@ var NewMapComponent = (function () {
       };
 
       var me = this;
-      if ((this.mapMode == "shapes") && (!_.isEmpty(this.shapeSource))) {
+      if (this.mapMode == "shapes") {
         var keys = _.pluck(json.resultset, idx.id);
         this.dataRequest(this.shapeSource, keys)
             .then(function () {
@@ -362,8 +362,8 @@ var NewMapComponent = (function () {
 
     dataRequest: function (url, keys) {
       var addIn = this.getAddIn('ShapeResolver', this.shapeResolver);
-      if (!addIn) {
-        if (this.shapeSource.endsWith('json')) {
+      if (!addIn && this.shapeSource) {
+        if (this.shapeSource.endsWith('json') || this.shapeSource.endsWith('js')) {
           addIn = this.getAddIn('ShapeResolver', 'simpleJSON');
         } else {
           addIn = this.getAddIn('ShapeResolver', 'kml');
