1b5071020cd9ea46d82e2660b061a3a6b55071b5
David Santos <david.santos@webdetails.pt>
2015-10-19 20:07:31 +0100

    [CDE-659] CDE save new dashboard: Description field isn't being considered

--- cde-core/test-js/legacy/cdf-dd-spec.js
+++ cde-core/test-js/legacy/cdf-dd-spec.js
@@ -25,4 +25,84 @@ describe("CDF-DD tests", function() {
     expect(cdfdd.isNewFile('/public/plugin-samples/pentaho-cdf-dd/cde_sample.wcdf')).toBeFalsy();
   });
 
+  describe("## combinedSaveAs Tests", function() {
+    beforeEach(function(done) {
+      spyOn(SaveRequests, "saveAsDashboard");
+      spyOn(SolutionTreeRequests, "getExplorerFolderEndpoint");
+      spyOn(CDFDDUtils, "promptNotification");
+
+      done()
+    });
+
+    afterEach(function(done) {
+      $(".layout-popup").remove();
+      done()
+    });
+
+    it("Default layout should be in dashboard mode", function() {
+      cdfdd.combinedSaveAs();
+
+      //Test if it is in dashboard save mode
+      expect($(".layout-popup #dashRadio").is(":checked")).toBe(true);
+      expect($(".layout-popup #widgetRadio").is(":checked")).toBe(false);
+
+      //Test if the elements visible are only the ones related to dashboard save mode
+      expect($(".layout-popup #saveAsFolderExplorer").is(":visible")).toBe(true);
+      expect($(".layout-popup #fileInput").is(":visible")).toBe(true);
+      expect($(".layout-popup #titleInput").is(":visible")).toBe(true);
+      expect($(".layout-popup #descriptionInput").is(":visible")).toBe(true);
+      expect($(".layout-popup #componentInput").is(":visible")).toBe(false);
+    });
+
+    it("should show error notification", function(done) {
+      cdfdd.combinedSaveAs();
+
+      //Values not properly defined
+      $(".layout-popup #fileInput").val("");
+      $(".layout-popup #titleInput").val("");
+      $(".layout-popup #descriptionInput").val("");
+
+      cdfdd.combinedSaveAsSubmit(true, "");
+
+      expect(SaveRequests.saveAsDashboard).not.toHaveBeenCalled();
+      expect(CDFDDUtils.promptNotification).toHaveBeenCalled();
+      expect(CDFDDUtils.promptNotification.calls.mostRecent().args[0]).toEqual("Error");
+
+      done();
+    });
+
+    it("Should call endpoint to save the dashboard", function() {
+      var folder = "dash_folder/";
+      var filename = "dash_filename.wcdf";
+
+      var saveAsParams = {
+        operation: "saveas",
+        file: folder + filename,
+        title: "dash_title",
+        description: "dash_description",
+        cdfstructure: "{}"
+      };
+
+      spyOn(cdfdd, "strip");
+      spyOn(JSON, "stringify").and.returnValue(saveAsParams.cdfstructure);
+
+      cdfdd.combinedSaveAs();
+
+      $(".layout-popup #fileInput").val(filename);
+      $(".layout-popup #titleInput").val(saveAsParams.title);
+      $(".layout-popup #descriptionInput").val(saveAsParams.description);
+
+      cdfdd.combinedSaveAsSubmit(true, folder);
+
+      expect(CDFDDUtils.promptNotification).not.toHaveBeenCalled();
+      expect(SaveRequests.saveAsDashboard).toHaveBeenCalled();
+
+      var mostRecentCalls = SaveRequests.saveAsDashboard.calls.mostRecent().args;
+
+      expect(mostRecentCalls[0]).toEqual(saveAsParams);
+      expect(mostRecentCalls[1]).toEqual(folder);
+      expect(mostRecentCalls[2]).toEqual(filename);
+      expect(mostRecentCalls[3]).toEqual(cdfdd);
+    })
+  })
 });
