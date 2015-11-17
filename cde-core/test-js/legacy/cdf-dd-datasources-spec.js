102cfa5780f09d8519f0aae7bc1ece1afb435c70
David Santos <david.santos@webdetails.pt>
2015-11-17 19:16:59 +0000

    [CDE-202] Datasources improvements on CDE

--- cde-core/test-js/legacy/cdf-dd-datasources-spec.js
+++ cde-core/test-js/legacy/cdf-dd-datasources-spec.js
@@ -11,21 +11,61 @@
  * the license for the specific language governing your rights and limitations.
  */
 
-describe("CDF-DD-DATASOURCES tests", function() {
+describe("CDF-DD-DATASOURCES tests", function () {
+  var panel;
+
+  beforeEach(function() {
+    panel = new DatasourcesPanel();
+    panel.datasourcesPallete = new PalleteManager(DatasourcesPanel.PALLETE);
+    panel.datasourcesTable = new TableManager(DatasourcesPanel.DATASOURCES);
+    panel.propertiesTable = new TableManager(DatasourcesPanel.PROPERTIES);
+  });
 
   it("Template Load Test - DataSources", function() {
+    spyOn(panel, "init");
+    panel.initTables();
+    expect(panel.initPallete).toBe(false);
+  });
 
-    var dsp = new DatasourcesPanel;
-    dsp.datasourcesPallete = new PalleteManager(DatasourcesPanel.PALLETE);
+  describe("Datasources Table Tests", function() {
+    var panelSourcesRows = {
+      oldLabel: {
+        type: "Label",
+        typeDesc: "Label Desc"
+      },
+      newLabel: {
+        type: "Label",
+        typeDesc: "Label Desc",
+        rowName: "Label Name"
+      },
+      oldSource: {
+        type: "DataSource",
+        typeDesc: "Source Desc"
+      },
+      newSource: {
+        type: "DataSource",
+        typeDesc: "Source Desc",
+        rowName: "Source Name"
+      }
+    };
 
-    spyOn(dsp, "init").and.callFake(function() {
-      //init datasources
+    beforeEach(function() {
+      spyOn(panel.datasourcesPallete, "setLinkedTableManager");
+      spyOn(panel.datasourcesTable, "init");
+      spyOn(cdfdd, "getDashboardData").and.returnValue({datasources: {rows: []}});
+
+      panel.initDataSourceTable();
     });
 
-    dsp.initTables();
+    it("Get DataSource Type Description Test", function() {
+      var columnExpressions = panel.datasourcesTable.getTableModel().getColumnGetExpressions();
+      var getDisplay = columnExpressions[0];
 
-    expect( dsp.initPallete ).toBe(false);
-
+      expect(getDisplay(panelSourcesRows.oldLabel)).toEqual("Label Desc");
+      expect(getDisplay(panelSourcesRows.newLabel)).toEqual("Label Desc");
+      expect(getDisplay(panelSourcesRows.oldSource)).toEqual("Source Desc");
+      expect(getDisplay(panelSourcesRows.newSource)).toEqual("Source Name");
+    });
   });
 
 });
