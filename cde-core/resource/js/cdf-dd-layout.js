7aa5bd2638e41ca236998d15bebc29fb7bb17ea7
João Velasques <joao.velasques@webdetails.pt>
2016-02-04 14:58:26 +0000

    [CDE-807] - Providing a shim so legacy libs in the editor can still use $.fn.attr to manipulate the checked property

--- cde-core/resource/js/cdf-dd-layout.js
+++ cde-core/resource/js/cdf-dd-layout.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2015 Webdetails, a Pentaho company. All rights reserved.
+ * Copyright 2002 - 2016 Webdetails, a Pentaho company. All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -622,8 +622,8 @@ var LayoutSaveAsTemplateOperation = SaveAsTemplateOperation.extend({
       submit: function(v) {
         title = $("#titleInput").val();
         file = $("#fileInput").val();
-        includeComponents = $("#includeComponentsInput").attr("checked");
-        includeDataSources = $("#includeDataSourcesInput").attr("checked");
+        includeComponents = $("#includeComponentsInput").prop("checked");
+        includeDataSources = $("#includeDataSourcesInput").prop("checked");
 
         var validate = true;
         if(file.length == 0) {
---
+++ cde-core/resource/js/jquery-shim.attr.js
@@ -0,0 +1,26 @@
+/*!
+ * Copyright 2002 - 2016 Webdetails, a Pentaho company. All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
+// The jquery.multiselect lib used by the editor uses $.fn.attr instead of $.fn.prop
+// to get/set the property "checked".
+// Making sure to redirect to the appropriate function when we detect this.
+(function() {
+  var originalAttr = $.fn.attr;
+  $.fn.attr = function() {
+    if(arguments[0] === "checked") {
+      // the editor code is not relying on string values on an attribute "checked"
+      // so its safe to just redirect attr to prop
+      return $.fn.prop.apply(this, arguments);
+    }
+    return originalAttr.apply(this, arguments);
+  }
+})();
