f04e3a11f17d4d8527def82fd6d838e8ee8cebce
Andrei Abramov <andrei_abramov@epam.com>
2016-11-09 16:29:14 +0400

    [SP-3039] Backport of CDE-849 - Export of table with paginate server side set as true is only exporting 10 results in a kettle data source (6.1 Suite) -backport of #739

--- cde-core/resource/resources/custom/components/exportButton/exportButton.js
+++ cde-core/resource/resources/custom/components/exportButton/exportButton.js
@@ -1,5 +1,15 @@
-
-
+/*!
+ * Copyright 2002 - 2016 Webdetails, a Pentaho company. All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
 
 var ExportButtonComponent = BaseComponent.extend({
 
@@ -37,7 +47,9 @@ var ExportButtonComponent = BaseComponent.extend({
   
   //add filtering to export if it's a data table
   getFilterSettings: function(component){
-    var extraSettings = {};
+    var extraSettings = {
+      exportPage: this.exportPage
+    };
 	if(component.type == "Table"){
 	  //var dtOptions = component.getTableOptions(component.chartDefinition);
 	  //if(dtOptions.bFilter) {
