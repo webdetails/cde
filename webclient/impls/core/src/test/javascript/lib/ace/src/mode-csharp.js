37e5410066f8bd6014c64847f99c580161587107
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 16:33:38 -0400

    HNC-766: updated license and copyright headers

--- webclient/impls/core/src/test/javascript/lib/ace/src/mode-csharp.js
+++ webclient/impls/core/src/test/javascript/lib/ace/src/mode-csharp.js
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 ace.define('ace/mode/csharp', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/mode/text', 'ace/tokenizer', 'ace/mode/csharp_highlight_rules', 'ace/mode/matching_brace_outdent', 'ace/mode/behaviour/cstyle', 'ace/mode/folding/cstyle'], function(require, exports, module) {
 
 
