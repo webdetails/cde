2db31076e19e9812c75d1ab80bcce4af7b1f9a92
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-29 15:09:13 -0400

    HNC-766: updated license and copyright headers

--- webclient/impls/core/src/main/javascript/insertAtCaret.js
+++ webclient/impls/core/src/main/javascript/insertAtCaret.js
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 // This is taken from https://gist.github.com/mathiasbynens/326491
 // @author Mathias Bynens <http://mathiasbynens.be/>
 
