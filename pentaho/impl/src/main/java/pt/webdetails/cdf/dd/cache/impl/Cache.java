1739820f8392d031208462bf775eab138f75b393
SahithiKommagani6 <sahithi.kommagani@hitachivantara.com>
2024-12-26 16:32:06 +0530

    [BACKLOG-41596]-Upgrading EhCache 2 to JCache and EhCache 3

--- pentaho/impl/src/main/java/pt/webdetails/cdf/dd/cache/impl/Cache.java
+++ pentaho/impl/src/main/java/pt/webdetails/cdf/dd/cache/impl/Cache.java
@@ -14,17 +14,21 @@
 package pt.webdetails.cdf.dd.cache.impl;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
-import net.sf.ehcache.CacheException;
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Ehcache;
-import net.sf.ehcache.Element;
 import pt.webdetails.cdf.dd.DashboardCacheKey;
 import pt.webdetails.cdf.dd.cache.api.ICache;
 import pt.webdetails.cdf.dd.model.inst.writer.cdfrunjs.dashboard.CdfRunJsDashboardWriteResult;
 import pt.webdetails.cpf.exceptions.InitializationException;
 import pt.webdetails.cpf.repository.api.IContentAccessFactory;
 
+import javax.cache.CacheException;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
 /**
  * Allows caching {@code CdfRunJsDashboardWriteResult} objects referenced by {@code DashboardCacheKey}.
  * Both these types are serializable.
@@ -35,41 +39,46 @@ import pt.webdetails.cpf.repository.api.IContentAccessFactory;
 public final class Cache implements ICache {
   private static final String CACHE_CFG_FILE = "ehcache.xml";
   private static final String CACHE_NAME = "pentaho-cde";
-  private Ehcache ehcache;
+  private static final String ENABLE_SHUTDOWN_HOOK_PROPERTY = "javax.cache.CacheManager.enableShutdownHook";
+  private javax.cache.Cache ehcache;
 
   public Cache( IContentAccessFactory contentAccessFactory ) throws InitializationException {
 
     CacheManager cacheManager;
     try {
-      cacheManager = CacheManager.create( contentAccessFactory.getPluginSystemReader( null )
-        .getFileInputStream( CACHE_CFG_FILE ) );
-    } catch ( IOException e ) {
-      throw new InitializationException( "Failed to load the cache configuration file: " + CACHE_CFG_FILE, e );
+      CachingProvider cachingProvider = Caching.getCachingProvider();
+      URI configUri = getClass().getClassLoader().getResource( CACHE_CFG_FILE ).toURI();
+      cacheManager = cachingProvider.getCacheManager( configUri, getClass().getClassLoader() );
     } catch ( CacheException e ) {
       throw new InitializationException( "Failed to create the cache manager.", e );
+    } catch ( URISyntaxException e ) {
+        throw new RuntimeException( "Failed to load the cache configuration file: " + CACHE_CFG_FILE, e );
     }
 
     // enableCacheProperShutdown
-    System.setProperty( CacheManager.ENABLE_SHUTDOWN_HOOK_PROPERTY, "true" );
+    System.setProperty( ENABLE_SHUTDOWN_HOOK_PROPERTY, "true" );
 
-    if ( !cacheManager.cacheExists( CACHE_NAME ) ) {
-      cacheManager.addCache( CACHE_NAME );
+    if ( cacheManager.getCache( CACHE_NAME ) == null ) {
+        MutableConfiguration<Object,Object> config = new MutableConfiguration<>().setStoreByValue( false );
+      cacheManager.createCache( CACHE_NAME, config );
     }
 
     this.ehcache = cacheManager.getCache( CACHE_NAME );
   }
 
   public CdfRunJsDashboardWriteResult get( DashboardCacheKey key ) {
-    Element element = this.ehcache.get( key );
+    Object element = this.ehcache.get( key );
 
     if ( element != null ) {
-      return (CdfRunJsDashboardWriteResult) element.getValue();
+      return (CdfRunJsDashboardWriteResult) element;
     }
     return null;
   }
 
   public List<DashboardCacheKey> getKeys() {
-    return (List<DashboardCacheKey>) this.ehcache.getKeys();
+    final List<DashboardCacheKey> keys = new ArrayList<>();
+    ehcache.iterator().forEachRemaining( entry -> keys.add( ( ( javax.cache.Cache.Entry<DashboardCacheKey, CdfRunJsDashboardWriteResult> ) entry ).getKey() ) );
+    return keys;
   }
 
   public void remove( DashboardCacheKey key ) {
@@ -81,7 +90,6 @@ public final class Cache implements ICache {
   }
 
   public void put( DashboardCacheKey key, CdfRunJsDashboardWriteResult value ) {
-    Element element = new Element( key, value );
-    this.ehcache.put( element );
+    this.ehcache.put( key, value );
   }
 }
